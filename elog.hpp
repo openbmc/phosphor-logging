43b636d7057f4ef7433842984b453bfad3d525d3
Adriana Kobylak <anoo@us.ibm.com>
2016-09-29 20:48:08 -0500

    WIP: Error / Event Log Framework
    Framework to create an error log entry based on information
    from the journal log.
    1. elog.yaml - User defined reason codes for error scenarios that
       include description, priority level, and desired metadata fields.
    2. elog_parser.py - Parser for the yaml file
    3. elog-gen.hpp - Auto-generated file based created by the parser
       that includes the error log structures. Including for now for ref.
    4. elog.hpp - Error log template to create (log a journal log
       entry with the predefined error log information).
    
    Usage: elog<RC>(NAME(value), prev_entry<NAME>()...)
      where RC - Reason code, NAME - metadata name
      being logged to a journal entry, prev_entry - A Metadata entry that
      has previously being logged to a journal entry.
    Example:
      log<level::DEBUG>(
            msg("Info trace to log filename"),
            entry(file_not_found::file_name::str, my_filename));
      elog<file_not_found>(file_not_found::errnum(2),
            file_not_found::file_path("/tmp/"),
            prev_entry<file_not_found::file_name>());
    
    Change-Id: Ic51cee80b58e341c071c366c5e2146fd6694012c
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>
    Signed-off-by: Andrew Geissler <andrewg@us.ibm.com>

---
+++ elog.hpp
@@ -0,0 +1,83 @@
+#pragma once
+
+#include <tuple>
+#include <utility>
+#include "elog-gen.hpp"
+
+namespace phosphor
+{
+
+namespace logging
+{
+
+// The prev_entry and deduce_entry_type structures below are used
+// to verify at compile time the required parameters have been passed to
+// the elog interface and then to also be able to forward the empty
+// prev_entry rvalue instances on to the log interface (which will
+// ignore them).
+template <typename T>
+struct prev_entry
+{
+    using type = T;
+};
+
+template <typename T>
+struct deduce_entry_type
+{
+
+    using type = T;
+    auto get() { return value._entry; }
+
+    T value;
+};
+
+template <typename T>
+struct deduce_entry_type<prev_entry<T>>
+{
+    using type = T;
+    auto get() { return std::make_tuple(); }
+
+    prev_entry<T> value;
+};
+
+template <typename T> using deduce_entry_type_t =
+        typename deduce_entry_type<T>::type;
+
+
+/** @fn commit()
+ *  @brief Create an error log entry based on journal
+ *          entry with a specified MSG_ID
+ *  @tparam E - Error log struct
+ */
+template <typename E>
+void commit()
+{
+    // TODO placeholder function call
+    // to call the new error log server to create
+    // an error log on the BMC flash
+    // dbus_commit(E.msgid); // call server
+}
+
+/** @fn elog()
+ *  @brief Create a journal log entry based on predefined
+ *          error log information
+ *  @tparam T - Error log type
+ *  @param[in] i_args - Metadata fields to be added to the journal entry
+ */
+template <typename T, typename ...Args>
+void elog(Args... i_args)
+{
+    // Validate the caller passed in the required parameters
+    static_assert(std::is_same<typename T::metadata_types,
+                                       std::tuple<deduce_entry_type_t<Args>...>>
+                                       ::value,
+                  "You are not passing in required arguments for this error");
+
+    log<T::L>(msg(T::err_msg),
+              deduce_entry_type<Args>{i_args}.get()...);
+}
+
+} // namespace logging
+
+} // namespace phosphor
+
