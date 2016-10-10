2a285457a6fd0854bfa713c2ae6db81e826f4426
Adriana Kobylak <anoo@us.ibm.com>
2016-10-10 11:39:20 -0500

    WIP: Create log manager server
    Error/event log dbus object with a placeholder method.
    
    Change-Id: Icfa3f0dcddb3f47a62d8480a936bd8baa4b49760
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

---
+++ log_manager.cpp
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <sdbusplus/vtable.hpp>
+#include <systemd/sd-bus.h>
+
+/*
+ * @fn commit()
+ * @brief Create an error/event log based on a message id
+ * @param[i] msg - dbus message
+ * @param[i] user_data - user data
+ * @param[i] error - dbus error
+ * @return Commit id
+ */
+auto commit(sd_bus_message *msg, void *user_data, sd_bus_error *error)
+{
+    int rc = 0;
+
+    return rc;
+}
+
+constexpr sdbusplus::vtable::vtable_t log_vtable[] =
+{
+    sdbusplus::vtable::start(),
+
+    sdbusplus::vtable::method("Commit", "i", "i", commit),
+    sdbusplus::vtable::end()
+};
+
+int main(int argc, char *argv[])
+{
+    constexpr const auto *dbus_log_obj = "/xyz/openbmc_project/log";
+    constexpr const auto *dbus_log_name = "xyz.openbmc_project.Log";
+    int rc = -1;
+    sd_bus *bus = NULL;
+    sd_bus_slot *slot = NULL;
+
+    rc = sd_bus_open_system(&bus);
+    if (rc < 0)
+    {
+        fprintf(stderr, "Failed to open system bus: %s\n", strerror(-rc));
+        goto cleanup;
+    }
+
+    rc = sd_bus_add_object_manager(bus, NULL, dbus_log_obj);
+    if (rc < 0)
+    {
+        fprintf(stderr, "Failed to add object mgr: %s\n", strerror(-rc));
+        goto cleanup;
+    }
+
+    rc = sd_bus_add_object_vtable(bus,
+                                  &slot,
+                                  dbus_log_obj,
+                                  dbus_log_name,
+                                  log_vtable,
+                                  NULL);
+    if (rc < 0)
+    {
+        fprintf(stderr, "Failed to add vtable: %s\n", strerror(-rc));
+        goto cleanup;
+    }
+
+    rc = sd_bus_request_name(bus, dbus_log_name, 0);
+    if (rc < 0)
+    {
+        fprintf(stderr, "Failed to acquire service name: %s\n", strerror(-rc));
+    }
+    else
+    {
+        for(;;)
+        {
+            rc = sd_bus_process(bus, NULL);
+            if (rc < 0)
+            {
+                fprintf(stderr, "Failed to connect to bus: %s\n", strerror(-rc));
+                break;
+            }
+            if (rc > 0)
+            {
+                continue;
+            }
+
+            rc = sd_bus_wait(bus, (uint64_t) - 1);
+            if (rc < 0)
+            {
+                fprintf(stderr, "Failed to wait on bus: %s\n", strerror(-rc));
+                break;
+            }
+        }
+    }
+
+cleanup:
+    slot = sd_bus_slot_unref(slot);
+    sd_bus_unref(bus);
+
+    return rc;
+}
+
