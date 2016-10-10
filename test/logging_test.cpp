1b23a202fcbee2673dcffe8f6a17434def33ec2f
Andrew Geissler <andrewg@us.ibm.com>
2016-10-10 12:43:48 -0500

    Add in test case which validates elog client interface
    Change-Id: I5c5f7a550a8f272251893ff616408c41d32281f9
    Signed-off-by: Andrew Geissler <andrewg@us.ibm.com>

--- test/logging_test.cpp
+++ test/logging_test.cpp
@@ -1,32 +1,90 @@
+// A basic unit test that runs on a BMC (qemu or hardware)
+
 #include <iostream>
 #include <elog.hpp>
 #include <log.hpp>
+#include <systemd/sd-journal.h>
 
 using namespace phosphor;
 using namespace logging;
 
+// validate the journal metadata equals the input value
+int validate_journal(const char *i_entry, const char *i_value)
+{
+    sd_journal *journal;
+    const void *data;
+    size_t l;
+    int rc;
+    bool validated = false;
+
+    rc = sd_journal_open(&journal, SD_JOURNAL_LOCAL_ONLY);
+    if (rc < 0) {
+            std::cerr << "Failed to open journal: " << strerror(-rc) << "\n";
+            return 1;
+    }
+    rc = sd_journal_query_unique(journal, i_entry);
+    if (rc < 0) {
+            std::cerr << "Failed to query journal: " << strerror(-rc) << "\n";
+            return 1;
+    }
+    SD_JOURNAL_FOREACH_UNIQUE(journal, data, l)
+    {
+        std::string journ_entry((const char*)data);
+        std::cout << journ_entry << "\n";
+        if(journ_entry.find(i_value) != std::string::npos)
+        {
+            std::cout << "We found it!\n";
+            validated = true;
+            break;
+        }
+    }
+
+    sd_journal_close(journal);
+
+    rc = (validated) ? 0 : 1;
+    if(rc)
+    {
+        std::cerr << "Failed to find " << i_entry << " in journal!" << "\n";
+    }
+
+    return rc;
+}
+
 int main()
 {
-    std::cout << "hello world!\n";
-
+    // TEST 1 - Basic log
     log<level::DEBUG>(
-            phosphor::logging::msg("Simple Example"));
+            phosphor::logging::msg("Basic phosphor logging test"));
 
-    const char *file_name = "HELLO.txt";
+    // TEST 2 - Log with metadata field
+    const char *file_name = "phosphor_logging_test.txt";
     int number = 0xFEFE;
-
     log<level::DEBUG>(
-            msg("THIS IS A PHOSPHOR LOGGING TEST"),
-            entry("FILE_NAME=%s_%x", file_name, number));
+            msg("phosphor logging test with attribute"),
+            entry("FILE_NAME_WITH_NUM_TEST=%s_%x", file_name, number));
 
-    number = 0xFFFF;
+    // Now read back and verify our data made it into the journal
+    int rc = validate_journal("FILE_NAME_WITH_NUM_TEST",
+                              "phosphor_logging_test.txt_fefe");
+    if(rc)
+        return(rc);
 
-    // TODO - next task
-    //struct FILE_NOT_FOUND_ERROR a;
-//    elog(a,
-//         (new_entry(ERRNO,99)),
-//         (new_entry(FILE_NAME,name))
-//         (prev_entry(FILE_NAME)));
+    // TEST 3 - Create error log with 2 meta data fields (rvalue and lvalue)
+    number = 0x1234;
+    elog(FILE_NOT_FOUND_ERROR(),
+         (new_entry(FILE_PATH,"/tmp/phosphor_elog_test.txt")),
+         (new_entry(ERRNO,number)));
+
+    // Now read back and verify our data made it into the journal
+    rc = validate_journal(FILE_PATH.md_name, "/tmp/phosphor_elog_test.txt");
+    if(rc)
+        return(rc);
+
+    rc = validate_journal(ERRNO.md_name, std::to_string(number).c_str());
+    if(rc)
+        return(rc);
 
     return 0;
 }
+
+
