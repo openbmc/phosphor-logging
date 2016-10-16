1f8c3342ac143e710a2ddc8e05556bee27bea117
Adriana Kobylak <anoo@us.ibm.com>
2016-10-16 09:54:40 -0500

    WIP: Add get journal data dbus method
    Find the most recent metadata value from the journal.
    
    Change-Id: Iaf944130c4075ae2d2c192e5ef840451561791bc
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- log_manager.c
+++ log_manager.c
@@ -1,5 +1,61 @@
 #include <stdio.h>
 #include <systemd/sd-bus.h>
+#include <systemd/sd-journal.h>
+
+/*
+ * @fn get_journal_data()
+ * @brief Get the most recent metadata value from a specified field name.
+ *        The returned data is prefixed with the specified field name and '='.
+ */
+static int get_journal_data(sd_bus_message *msg,
+                            void *user_data,
+                            sd_bus_error *error)
+{
+    int rc = -1;
+    char *var = NULL;
+    char reply[64*1024]; // sd_journal_get_data() truncates data at 64K
+    sd_journal *j;
+
+    reply[0] = '\0';
+
+    // Metatdata field name the caller wants to find
+    rc = sd_bus_message_read(msg, "s", &var);
+    if (rc < 0)
+    {
+        fprintf(stderr, "Failed to read msg data: %s\n", strerror(-rc));
+        return sd_bus_reply_method_return(msg, "i", rc);
+    }
+
+    rc = sd_journal_open(&j, SD_JOURNAL_LOCAL_ONLY);
+    if (rc < 0)
+    {
+        fprintf(stderr, "Failed to open journal: %s\n", strerror(-rc));
+        return sd_bus_reply_method_return(msg, "i", rc);
+    }
+
+    // Find the most recent instance by reading each journal entry starting
+    // with the end of the journal
+    SD_JOURNAL_FOREACH_BACKWARDS(j) 
+    {
+        const char *d = NULL;
+        size_t l = 0;
+
+        rc = sd_journal_get_data(j, var, (const void **)&d, &l);
+        if (rc < 0) 
+        {
+            // Instance not found, continue to next journal entry
+            continue;
+        }
+        // Copy match to reply variable since journal fd will be closed
+        snprintf(reply, l, "%s", d);
+        break;
+    }
+    sd_journal_close(j);
+
+    // No need to check that reply has been populated, if no match was found
+    // then rc is set to -2
+    return sd_bus_reply_method_return(msg, "is", rc, reply);
+}
 
 /*
  * @fn commit()
@@ -16,6 +72,8 @@ static const sd_bus_vtable log_vtable[] =
 {
     SD_BUS_VTABLE_START(0),
     SD_BUS_METHOD("commit", "", "i", &commit, SD_BUS_VTABLE_UNPRIVILEGED),
+    SD_BUS_METHOD("GetJournalData", "s", "is", &get_journal_data,
+                    SD_BUS_VTABLE_UNPRIVILEGED),
     SD_BUS_VTABLE_END
 };
 
