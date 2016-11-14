c1f50fedd5bb784f490624dcce46d22d37aedb0d
Adriana Kobylak <anoo@us.ibm.com>
2016-11-14 14:46:23 -0600

    log_manager: Use sdbus++
    Change-Id: Id2ad6a75b6ee3f7226c7cfd274dba24de89f9363
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

---
+++ log_manager.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <sdbusplus/server.hpp>
+#include <xyz/openbmc_project/Logging/Manager/server.hpp>
+
+namespace phosphor
+{
+namespace logging
+{
+
+/** @class Manager
+ *  @brief OpenBMC logging manager implementation.
+ *  @details A concrete implementation for xyz.openbmc_project.Logging.Manager
+ *  DBus API.
+ */
+class Manager : public sdbusplus::server::xyz::openbmc_project::Logging::Manager
+{
+    public:
+        Manager() = delete;
+        Manager(const Manager&) = delete;
+        Manager& operator=(const Manager&) = delete;
+        Manager(Manager&&) = default;
+        Manager& operator=(Manager&&) = default;
+        ~Manager() = default;
+        Manager(const char *, const char*, const char*);
+
+        /** @brief Start processing DBus messages. */
+        void run() noexcept;
+
+        /** @brief sd_bus Commit method implementation callback. */
+        void commit(int msgid, std::string emsg) override;
+
+    private:
+        /** @brief Persistent sdbusplus DBus bus connection. */
+        sdbusplus::bus::bus _bus;
+};
+
+} // namespace logging
+} // namespace phosphor
---
+++ log_manager_main.cpp
@@ -0,0 +1,23 @@
+#include "config.h"
+#include "log_manager.hpp"
+#include <sdbusplus/bus.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <exception>
+
+int main(int argc, char *argv[])
+{
+    try {
+        auto manager = phosphor::logging::manager::Manager(
+                sdbusplus::bus::new_system(),
+                BUSNAME,
+                INVENTORY_ROOT,
+                IFACE);
+        manager.run();
+        exit(EXIT_SUCCESS);
+    }
+    catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+    exit(EXIT_FAILURE);
+}
