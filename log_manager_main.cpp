c480166802ad4815b86e8115b251df6d103a98df
Adriana Kobylak <anoo@us.ibm.com>
2016-12-11 10:05:16 -0600

    logging: Create Entry dbus interface
    Implement the generated code to create an error/event
    entry dbus object.
    
    Change-Id: I881636fe3e8de680d9a15fff1fe933d5e22eed06
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- log_manager_main.cpp
+++ log_manager_main.cpp
@@ -10,9 +10,21 @@ int main(int argc, char *argv[])
     try {
         auto manager = phosphor::logging::Manager(
                 sdbusplus::bus::new_system(),
-                BUSNAME,
-                OBJ);
+                BUSNAME_INTERNAL,
+                OBJ_INTERNAL);
+
+        auto bus = sdbusplus::bus::new_default();
+
+        // Add sdbusplus ObjectManager.
+        sdbusplus::server::manager::manager(bus, OBJ_ENTRY);
+
+        bus.request_name(BUSNAME_ENTRY);
+
+        // TODO Create error log dbus object on demand, when the Commit interface
+        // creates an error log it'd call this entry interface to create an object.
+
         manager.run();
+
         exit(EXIT_SUCCESS);
     }
     catch (const std::exception &e) {
