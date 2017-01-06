d70b81a819fd0fc0be4d1c2d5d599baec0f2dd5b
Adriana Kobylak <anoo@us.ibm.com>
2017-01-06 11:56:41 -0600

    log_manager: Cleanup main function
    The design direction changed after the commit that added the
    log_manager main function, so updating it to the correct usage.
    
    Change-Id: I88459fb65ba6b8272f0f9f85b55f9f837487ffea
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- log_manager_main.cpp
+++ log_manager_main.cpp
@@ -1,34 +1,29 @@
+#include <iostream>
 #include "config.h"
 #include "log_manager.hpp"
-#include <sdbusplus/bus.hpp>
-#include <cstdlib>
-#include <iostream>
-#include <exception>
 
 int main(int argc, char *argv[])
 {
-    try {
-        auto manager = phosphor::logging::Manager(
-                sdbusplus::bus::new_system(),
-                BUSNAME_INTERNAL,
-                OBJ_INTERNAL);
+    auto busManager = sdbusplus::bus::new_default();
+    auto busEntry = sdbusplus::bus::new_default();
 
-        auto bus = sdbusplus::bus::new_default();
+    phosphor::logging::Manager manager(busManager, OBJ_INTERNAL);
 
-        // Add sdbusplus ObjectManager.
-        sdbusplus::server::manager::manager(bus, OBJ_ENTRY);
+    // Add sdbusplus ObjectManager.
+    sdbusplus::server::manager::manager(busManager, OBJ_INTERNAL);
+    sdbusplus::server::manager::manager(busEntry, OBJ_ENTRY);
 
-        bus.request_name(BUSNAME_ENTRY);
+    busManager.request_name(BUSNAME_INTERNAL);
+    busEntry.request_name(BUSNAME_ENTRY);
 
-        // TODO Create error log dbus object on demand, when the Commit interface
-        // creates an error log it'd call this entry interface to create an object.
+    // TODO Create error log dbus object on demand, when the Commit interface
+    // creates an error log it'd call this entry interface to create an object.
 
-        manager.run();
-
-        exit(EXIT_SUCCESS);
+    while(true)
+    {
+        busManager.process_discard();
+        busManager.wait();
     }
-    catch (const std::exception &e) {
-        std::cerr << e.what() << std::endl;
-    }
-    exit(EXIT_FAILURE);
+
+    return 0;
 }
