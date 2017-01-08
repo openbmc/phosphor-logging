bad2a757024c694d3c585b2817de2782b9ebcfa3
Adriana Kobylak <anoo@us.ibm.com>
2017-01-08 15:14:02 -0600

    Entry: Setup constructor
    Persist the log manager dbus bus and create a
    vector of Entry instances to store the Entry
    dbus objects as they get created.
    
    Change-Id: I4add43c4ce6795b6ec6c041e41cd7455d34b3b6b
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- elog_entry.cpp
+++ elog_entry.cpp
@@ -6,12 +6,6 @@ namespace phosphor
 namespace logging
 {
 
-Entry::Entry(sdbusplus::bus::bus& bus, const char* obj) :
-    details::ServerObject<details::EntryIface>(bus, obj)
-{
-    // TODO Add logic to populate error log dbus objects that exist on flash
-}
-
 // TODO Add interfaces to handle the error log id numbering
 
 } // namespace logging
