31beed21fde8135e3b5efea21710a0c0e90dd9dd
Michael Tritz <mtritz@us.ibm.com>
2017-02-19 20:57:46 -0600

    Error log inject tool
    This tool adds an option to the logging-test
    to inject an error as specified by command
    line.
    
    Change-Id: I1028bd3964db23ed5ffa50414f7a6cc2f880c40c
    Signed-off-by: Michael Tritz <mtritz@us.ibm.com>

--- logging_test.cpp
+++ logging_test.cpp
@@ -1,5 +1,6 @@
 // A basic unit test that runs on a BMC (qemu or hardware)
 
+#include <getopt.h>
 #include <iostream>
 #include <systemd/sd-journal.h>
 #include <sstream>
@@ -9,6 +10,15 @@
 
 using namespace phosphor;
 using namespace logging;
+
+//TODO: more verbose
+const char *usage = "Usage: logging-test [OPTION]          \n\n\
+Options:                                                     \n\
+[NONE]                          Complete error-logging test. \n\
+-h, --help                      Display this usage text.     \n\
+-c, --commit <string>           Commit desired error.      \n\n\
+Valid errors to commit:                                      \n\
+AutoTestSimple\n";
 
 // validate the journal metadata equals the input value
 int validate_journal(const char *i_entry, const char *i_value)
@@ -53,7 +63,7 @@ int validate_journal(const char *i_entry, const char *i_value)
     return rc;
 }
 
-int main()
+int elog_test()
 {
     // TEST 1 - Basic log
     log<level::DEBUG>("Basic phosphor logging test");
@@ -194,4 +204,58 @@ int main()
     return 0;
 }
 
+void commitError(const char *text)
+{
+    if (strcmp(text, "AutoTestSimple") == 0)
+    {
+        try
+        {
+            elog<example::xyz::openbmc_project::Example::AutoTestSimple>(
+                example::xyz::openbmc_project::Example::AutoTestSimple::
+                    STRING("FOO"));
+        }
+        catch (elogException<example::xyz::openbmc_project::
+            Example::AutoTestSimple>& e)
+        {
+            std::cout << "elog exception caught: " << e.what() << std::endl;
+            commit(e.name());
+        }
+    }
 
+    return;
+}
+
+int main(int argc, char *argv[])
+{
+    char arg;
+
+    if (argc == 1)
+        return elog_test();
+
+    static struct option long_options[] =
+    {
+          {"help",    no_argument,       0, 'h'},
+          {"commit",  required_argument, 0, 'c'},
+          {0, 0, 0, 0}
+    };
+    int option_index = 0;
+
+    while((arg=getopt_long(argc,argv,"hc:",long_options,&option_index)) != -1)
+    {
+        switch (arg)
+        {
+            case 'c':
+                commitError(optarg);
+                return 0;
+
+            case 'h':
+            case '?':
+                std::cerr << usage;
+                return 1;
+
+        }
+    }
+
+    return 0;
+
+}
