5ac00af9b6fcc40f05dec78ccf3ea989c538b00e
Deepak Kodihalli <dkodihal@in.ibm.com>
2017-02-27 07:07:33 -0600

    Add interface to process metadata
    A metadata field found in the journal corresponding to an error could be
    special in the sense that it may need to processed before constructing
    the error d-bus object. Add an interface which can be implemented for
    specific metadata processing.
    
    Take advantage of the fact that every metadata has a corresponding type
    defined.
    
    Change-Id: I09e446879667044c009141443fc0dc6fed8a8b18
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>

---
+++ elog_meta.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <phosphor-logging/elog-errors.hpp>
+#include "elog_entry.hpp"
+
+namespace phosphor
+{
+namespace logging
+{
+namespace metadata
+{
+
+using Metadata = std::string;
+using HandlerType = void(const std::vector<std::string>&,
+                         AssociationList& list);
+
+/** @brief Metadata handler, specialize this template to implement
+ *         handling a specific type of metadata.
+ *  @tparam M - type of metadata
+ *  @param [in] data - metadata key=value entries
+ *  @param [out] list - list of error association objects
+ */
+template <typename M>
+void handler(const std::vector<std::string>& data,
+             AssociationList& list) = delete;
+
+} // namespace metadata
+} // namespace logging
+} // namespace phosphor
