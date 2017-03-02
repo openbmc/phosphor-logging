0d002f452a0774562bf8252dd0a719cd26c9e662
Deepak Kodihalli <dkodihal@in.ibm.com>
2017-03-02 03:07:10 -0600

    Add utility to parse error metadata
    Add method to parse out the metadata name and value from the error
    metadata, which is of the format "name=value".
    
    Change-Id: I7b6ba790c97faced664a11eb5806f7f87e8166a7
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>

--- elog_meta.hpp
+++ elog_meta.hpp
@@ -17,6 +17,27 @@ using HandlerType = void(std::string&&,
                          const std::vector<std::string>&,
                          AssociationList& list);
 
+/** @brief Pull out metadata name and value from the string
+ *         <metadata name>=<metadata value>
+ *  @param [in] data - metadata key=value entries
+ *  @param [out] metadata - map of metadata name:value
+ */
+auto parse = [](const std::vector<std::string>& data,
+                std::map<std::string, std::string>& metadata)
+{
+    constexpr auto separator = "=";
+    for(const auto& entry: data)
+    {
+        auto pos = entry.find(separator);
+        if(std::string::npos != pos)
+        {
+            auto key = entry.substr(0, entry.find(separator));
+            auto value = entry.substr(entry.find(separator) + 1);
+            metadata[std::move(key)] = std::move(value);
+        }
+    }
+};
+
 /** @brief Metadata handler, specialize this template to implement
  *         handling a specific type of metadata.
  *  @tparam M - type of metadata
