989401b435c1fedea50bd4afa228d6eb091cf1a7
Deepak Kodihalli <dkodihal@in.ibm.com>
2017-03-05 23:23:50 -0600

    test: add callout-test
    Add test program which can create callouts based on an input device
    path. It creates an error, with callouts, with name TestCallout.
    
    Change-Id: I96b66b73ae4a9c00daff06222841a13747c07408
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>

---
+++ callouts/callout_test.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <phosphor-logging/elog.hpp>
+#include <phosphor-logging/elog-errors.hpp>
+#include "elog_meta.hpp"
+
+using namespace phosphor::logging;
+using namespace metadata;
+
+int main(int argc, char** argv)
+{
+    if(2 != argc)
+    {
+        std::cerr << "usage: callout-test <sysfs path>" << std::endl;
+        return -1;
+    }
+
+    try
+    {
+        using namespace example::xyz::openbmc_project::Example::Elog;
+        elog<TestCallout>(
+            TestCallout::DEV_ADDR(0xDEADEAD),
+            TestCallout::CALLOUT_ERRNO_TEST(0),
+            TestCallout::CALLOUT_DEVICE_PATH_TEST(argv[1]));
+    }
+    catch (elogException<TestCallout>& e)
+    {
+        commit(e.name());
+    }
+
+    return 0;
+}
+
+
