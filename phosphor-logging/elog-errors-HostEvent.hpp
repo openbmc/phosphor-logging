41906358474a886e875ccf1af99a5a247c7cacfd
Marri Devender Rao <devenrao@in.ibm.com>
2017-04-04 03:43:00 -0500

    Add new commit API
    Added new commit method that accepts an sdbusplus exception.
    
    Change-Id: I25c2b8f623493c46108732d6cfb3354bc401abee
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

--- phosphor-logging/elog-errors-HostEvent.hpp
+++ phosphor-logging/elog-errors-HostEvent.hpp
@@ -46,8 +46,8 @@ struct ESEL
 
 struct Event
 {
-    static constexpr auto err_code = "org.open_power.Error.Host.Event.Event";
-    static constexpr auto err_msg = "A host system event was received";
+    static constexpr auto errName = "org.open_power.Error.Host.Event.Event";
+    static constexpr auto errDesc = "A host system event was received";
     static constexpr auto L = level::INFO;
     using ESEL = _Event::ESEL;
     using metadata_types = std::tuple<ESEL>;
