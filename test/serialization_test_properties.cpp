608f649dca4725e31ee68d9969076d39aa9bbfe7
Deepak Kodihalli <dkodihal@in.ibm.com>
2017-06-14 03:56:12 -0500

    serialization: add tests
    Resolves openbmc/openbmc#1684.
    
    Change-Id: Ia554147001e51b05fe8692ae0b39e3efaf481130
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>

---
+++ test/serialization_test_properties.cpp
@@ -0,0 +1,53 @@
+#include "serialization_tests.hpp"
+#include "elog_entry.hpp"
+#include "elog_serialize.hpp"
+
+namespace phosphor
+{
+namespace logging
+{
+namespace test
+{
+
+TEST_F(TestSerialization, testProperties)
+{
+    auto id = 99;
+    phosphor::logging::AssociationList assocations{};
+    std::vector<std::string> testData{"additional", "data"};
+    uint64_t timestamp{100};
+    std::string message{"test error"};
+    auto input = std::make_unique<Entry>(
+                     bus,
+                     std::string(OBJ_ENTRY) + '/' + std::to_string(id),
+                     id,
+                     timestamp,
+                     Entry::Level::Informational,
+                     std::move(message),
+                     std::move(testData),
+                     std::move(assocations),
+                     manager);
+    auto path = serialize(*input, TestSerialization::dir);
+
+    auto idStr = path.filename().c_str();
+    id = std::stol(idStr);
+    auto output = std::make_unique<Entry>(
+                      bus,
+                      std::string(OBJ_ENTRY) + '/' + idStr,
+                      id,
+                      manager);
+    deserialize(path, *output);
+
+    EXPECT_EQ(input->id(), output->id());
+    EXPECT_EQ(input->severity(), output->severity());
+    EXPECT_EQ(input->timestamp(), output->timestamp());
+    EXPECT_EQ(input->message(), output->message());
+    EXPECT_EQ(input->additionalData(), output->additionalData());
+    EXPECT_EQ(input->resolved(), output->resolved());
+    EXPECT_EQ(input->associations(), output->associations());
+}
+
+} // namespace test
+} // namespace logging
+} // namespace phosphor
+
+
---
+++ test/serialization_tests.hpp
@@ -0,0 +1,41 @@
+#include <gtest/gtest.h>
+#include <experimental/filesystem>
+#include <stdlib.h>
+#include <sdbusplus/bus.hpp>
+#include "log_manager.hpp"
+#include "config.h"
+
+namespace phosphor
+{
+namespace logging
+{
+namespace test
+{
+
+namespace fs = std::experimental::filesystem;
+
+char tmplt[] = "/tmp/logging_test.XXXXXX";
+auto bus = sdbusplus::bus::new_default();
+phosphor::logging::Manager manager(bus, OBJ_INTERNAL);
+
+class TestSerialization : public testing::Test
+{
+    public:
+        TestSerialization() :
+            dir(fs::path(mkdtemp(tmplt)))
+        {
+        }
+
+        ~TestSerialization()
+        {
+            fs::remove_all(dir);
+        }
+
+        fs::path dir;
+};
+
+} // namespace test
+} // namespace logging
+} // namespace phosphor
+
+
