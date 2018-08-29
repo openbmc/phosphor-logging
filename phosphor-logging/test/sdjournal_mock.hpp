9f9a522cefa17b124dc66bd78a62353f7232222a
Patrick Venture <venture@google.com>
2018-08-29 11:41:01 -0700

    add sdjournal interface to inject tests
    The goal of the tests is not to test phosphor-logging, but rather allow
    code to call through phosphor-logging/log<> during a test.
    
    Change-Id: Id8c84ded473decc7f9f0be268116083093f86e54
    Signed-off-by: Patrick Venture <venture@google.com>

---
+++ phosphor-logging/test/sdjournal_mock.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <phosphor-logging/sdjournal.hpp>
+
+#include <gmock/gmock.h>
+
+namespace phosphor
+{
+namespace logging
+{
+
+class SdJournalMock : public SdJournalImpl
+{
+  public:
+    virtual ~SdJournalMock() = default;
+
+    /* Set your mock to catch this call. */
+    MOCK_METHOD(journal_send_call, int(const char*));
+
+    int journal_send(const char* fmt, ...) override
+    {
+        return journal_send_call(fmt);
+    }
+}
+
+} // namespace logging
+} // namespace phosphor
