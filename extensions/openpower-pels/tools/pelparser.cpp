1e1c1ff2b544d1b8570f555c01dbc718d435d25a
Aatir Manzur <aatrapps@gmail.com>
2019-08-22 11:01:41 -0500

    PEL Tool
    PEL Tool would be used for parsing PELs into human readable form.
    
    WIP
    
    Signed-off-by: Aatir Manzur <aatrapps@gmail.com>
    Change-Id: Ic0813c3c51c31260d32ce6304139449e7dee2f59

---
+++ extensions/openpower-pels/tools/pelparser.cpp
@@ -0,0 +1,68 @@
+#include "pelparser.hpp"
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+#include "../pel.hpp"
+#include "extensions/openpower-pels/paths.hpp"
+#include "extensions/openpower-pels/repository.hpp"
+#include "extensions/openpower-pels/tools/peltool_utility.hpp"
+#include "extensions/openpower-pels/user_header.hpp"
+#include "peltool_utility.hpp"
+#include "test/openpower-pels/pel_utils.hpp"
+
+#include <CLI/CLI.hpp>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <iostream> // std::cout
+#include <iterator>
+#include <map>
+#include <phosphor-logging/log.hpp>
+#include <sstream> // std::stringstream
+#include <string>  // std::string
+>>>>>>> 352a62f9b... phosphor-logging: Add depends for openpower-pels
+=======
+>>>>>>> a94e43983... PEL Tool
+using namespace phosphor::logging;
+namespace fs = std::filesystem;
+using namespace openpower::pels;
+
+namespace openpower
+{
+namespace pels
+{
+
+/*****************************************************************************/
+// Failing SubSystem
+/*****************************************************************************/
+void PelParser::PrintFailingSubSystem(uint8_t i_val)
+{
+    //    PrintTableItem((uint32_t)i_val, ErrlSubSysTable, ErrlSubSysTableSz);
+}
+
+/*****************************************************************************/
+// Severity
+/*****************************************************************************/
+void PelParser::PrintSeverity(uint8_t i_val)
+{
+    //    PrintTableItem((uint32_t)i_val, ErrlSevTable, ErrlSevTableSz);
+}
+
+/*****************************************************************************/
+// Domain
+/*****************************************************************************/
+void PelParser::PrintDomain(uint8_t i_val)
+{
+    //    PrintTableItem((uint32_t)i_val, ErrlDomainTable, ErrlDomainTableSz);
+}
+
+/*****************************************************************************/
+// Vector
+/*****************************************************************************/
+void PelParser::PrintVector(uint8_t i_val)
+{
+    //    PrintTableItem((uint32_t)i_val, ErrlVectorTable, ErrlVectorTableSz);
+}
+} // namespace pels
+} // namespace openpower
