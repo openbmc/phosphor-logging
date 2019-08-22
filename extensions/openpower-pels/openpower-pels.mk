f7381c3ffc52b1f54deded09b2183326f313bd94
Aatir Manzur <aatrapps@gmail.com>
2019-08-22 11:01:41 -0500

    PEL Tool
    PEL Tool would be used for parsing PELs into human readable form.
    
    WIP
    
    Signed-off-by: Aatir Manzur <aatrapps@gmail.com>
    Change-Id: Ic0813c3c51c31260d32ce6304139449e7dee2f59

--- extensions/openpower-pels/openpower-pels.mk
+++ extensions/openpower-pels/openpower-pels.mk
@@ -9,3 +9,33 @@ phosphor_log_manager_SOURCES += \
 	extensions/openpower-pels/private_header.cpp \
 	extensions/openpower-pels/repository.cpp \
 	extensions/openpower-pels/user_header.cpp
+
+bin_PROGRAMS += peltool
+
+peltool_SOURCES = extensions/openpower-pels/tools/peltool.cpp \
+       extensions/openpower-pels/tools/peltool_utility.cpp \
+       extensions/openpower-pels/tools/pelparser.cpp \
+       test/openpower-pels/pel_utils.cpp
+
+peltool_LDADD = \
+    extensions/openpower-pels/pel.o \
+    extensions/openpower-pels/pel_values.o \
+    extensions/openpower-pels/bcd_time.o \
+    extensions/openpower-pels/private_header.o \
+    extensions/openpower-pels/user_header.o \
+    extensions/openpower-pels/log_id.o \
+    extensions/openpower-pels/paths.o
+
+peltool_LDFLAGS = \
+        $(SYSTEMD_LIBS) \
+        $(PHOSPHOR_LOGGING_LIBS) \
+        $(SDBUSPLUS_LIBS) \
+        $(PHOSPHOR_DBUS_INTERFACES_LIBS) \
+        $(SDEVENTPLUS_LIBS) \
+        -lstdc++fs
+
+peltool_CXXFLAGS = \
+        $(SYSTEMD_CFLAGS) \
+        $(SDBUSPLUS_CFLAGS) \
+        $(SDEVENTPLUS_CFLAGS) \
+        $(PHOSPHOR_DBUS_INTERFACES_CFLAGS)
