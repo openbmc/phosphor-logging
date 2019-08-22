85a66a89cf9911683535d64468b07909cd027fe4
Aatir Manzur <aatrapps@gmail.com>
2019-08-22 11:01:41 -0500

    PEL Tool
    PEL Tool would be used for parsing PELs into human readable form.
    
    WIP
    
    Signed-off-by: Aatir Manzur <aatrapps@gmail.com>
    Change-Id: Ic0813c3c51c31260d32ce6304139449e7dee2f59

--- extensions/openpower-pels/user_header.cpp
+++ extensions/openpower-pels/user_header.cpp
@@ -10,6 +10,7 @@ namespace pels
 {
 
 using namespace phosphor::logging;
+using json = nlohmann::json;
 
 void UserHeader::unflatten(Stream& stream)
 {
@@ -40,6 +41,11 @@ UserHeader::UserHeader(Stream& pel)
     }
 }
 
+/**
+ * @brief Validates the section contents
+ *
+ * Updates _valid (in Section) with the results.
+ */
 void UserHeader::validate()
 {
     bool failed = false;
@@ -58,6 +64,35 @@ void UserHeader::validate()
     }
 
     _valid = (failed) ? false : true;
+}
+
+/**
+ * @brief Returns the section header in json format.
+ *
+ * @return  char *
+ */
+const char* UserHeader::toJson()
+{
+    auto tmp = pel_values::findByValue(this->_eventSeverity,
+                                       pel_values::severityValues);
+    const char* severity = std::get<pel_values::registryNamePos>(*tmp);
+    // TODO FOR ALL OTHER FIELDS
+    auto uh = R"(
+    {
+        "Section Version": "",
+        "Sub-section type": "",
+        "Log Committed by": "",
+        "Subsystem": "",
+        "Event Scope": "",
+        "Event Severity":""
+    }
+    )"_json;
+    // any Hex dumps would be added here too
+    uh["Event Severity"] = severity;
+    std::stringstream ss;
+    ss << "TODO" << uh.dump();
+    const char* c = &*ss.str().begin();
+    return c;
 }
 
 } // namespace pels
