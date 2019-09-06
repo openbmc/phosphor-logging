aa41f0cf46f44f30654d09fb90d4c314d449a4a4
Aatir Manzur <aatrapps@gmail.com>
2019-09-06 13:30:48 -0500

    PEL: User Data class
    This class is used for accessing user data section of a PEL.
    
    Signed-off-by: Aatir Manzur <aatrapps@gmail.com>
    Change-Id: I1223f84353e81202d1ff63c00f3d926cda4994e5

---
+++ extensions/openpower-pels/user_data.cpp
@@ -0,0 +1,68 @@
+#include "user_data.hpp"
+
+#include <phosphor-logging/log.hpp>
+
+namespace openpower
+{
+namespace pels
+{
+
+using namespace phosphor::logging;
+
+Stream& operator>>(Stream& s, vector<uint8_t> v)
+{
+    for (int i = 0; i < v.size(); ++i)
+    {
+        s << v[i];
+    }
+    return s;
+}
+void UserData::unflatten(Stream& stream)
+{
+    stream >> _header >> _data;
+}
+
+void UserData::flatten(Stream& stream)
+{
+    stream << _header << _data;
+}
+
+UserData::UserData(Stream& pel)
+{
+    try
+    {
+        unflatten(pel);
+        validate();
+    }
+    catch (const std::exception& e)
+    {
+        log<level::ERR>("Cannot unflatten user data",
+                        entry("ERROR=%s", e.what()));
+        _valid = false;
+    }
+}
+
+void UserData::validate()
+{
+    bool failed = false;
+    if (header().id != userDataSectionID)
+    {
+        log<level::ERR>("Invalid user data section ID",
+                        entry("ID=0x%X", header().id));
+        failed = true;
+        return;
+    }
+
+    if (header().version != userDataVersion)
+    {
+        log<level::ERR>("Invalid user data version",
+                        entry("VERSION=0x%X", header().version));
+        failed = true;
+        return;
+    }
+
+    _valid = (failed) ? false : true;
+}
+
+} // namespace pels
+} // namespace openpower
