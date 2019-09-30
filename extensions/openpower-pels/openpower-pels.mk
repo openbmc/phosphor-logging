6852d7222d9268d2b205672d6296424c0581c830
Matt Spinler <spinler@us.ibm.com>
2019-09-30 15:35:53 -0500

    PEL: Represent the SRC ASCII string field
    In the SRC section of a PEL, there is a field called the 'ASCII string'.
    This is the string of 32 characters that shows up on the panel when the
    SRC function is chosen, and usually when people refer to an SRC, the
    first 8 characters of this field is what they are referring to.
    
    This new class handles that string.  It will belong to the SRC section
    object.
    
    For BMC error SRCs, it looks like: BDSSRRRR
    Where:
        BD = "BD", indicating a BMC error SRC
        SS = subsystem value from PEL spec
        RRRR = reason code of the error
    
    The remaining 24 characters are spaces (' ').
    
    For example:
        "BD8D1234                        "
    
    For BMC power* related errors, the value is:
    
        "11001234                        "
    
    Where the difference is the "11" instead of "BD", and the following
    2 bytes are always "00".
    
    * 'power' means comes from the repository that monitors for power
      faults.  This is different purely to help keep field service
      documentation the same as in previous IBM server generations.
    
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>
    Change-Id: I6e7292e7f5b501428999781b1a5ee5c243a63ac6

--- extensions/openpower-pels/openpower-pels.mk
+++ extensions/openpower-pels/openpower-pels.mk
@@ -1,4 +1,5 @@
 phosphor_log_manager_SOURCES += \
+	extensions/openpower-pels/ascii_string.cpp \
 	extensions/openpower-pels/bcd_time.cpp \
 	extensions/openpower-pels/data_interface.cpp \
 	extensions/openpower-pels/entry_points.cpp \
