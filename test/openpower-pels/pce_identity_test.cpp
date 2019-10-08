41061a895de9de3717d2e16ce797f49c11558f27
Matt Spinler <spinler@us.ibm.com>
2019-10-08 14:13:31 -0500

    PEL: Power controlling enc SRC substructure
    This substructure is part of the callout subsection in the SRC section
    of a PEL, and contains enclosure information for when another enclosure
    controls the power of the failing entity.  This would be an unusual
    case, when the piece of hardware that is being called out has its power
    controlled by another enclosure, for example when an I/O expansion
    drawer is connected to 2 servers, and only one of them controls its
    power.
    
    This includes:
    * The enclosure's name
    * The enclosure's machine type, model, and serial number
    
    The BMC will never create this section for BMC errors, but it may need
    to unflatten them for PELs sent down from a host that has to deal with
    I/O drawers.
    
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>
    Change-Id: Ie04c1ee3fdfa67ee8666c10fa3bc837f4d33a9ef

---
+++ test/openpower-pels/pce_identity_test.cpp
@@ -0,0 +1,42 @@
+#include "extensions/openpower-pels/pce_identity.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace openpower::pels;
+using namespace openpower::pels::src;
+
+TEST(PCEIdentityTest, TestConstructor)
+{
+    std::vector<uint8_t> data{
+        'P', 'E', 0x24, 0x00,                      // type, size, flags
+        'T', 'T', 'T',  'T',  '-', 'M', 'M',  'M', // MTM
+        '1', '2', '3',  '4',  '5', '6', '7',       // SN
+        '8', '9', 'A',  'B',  'C', 'P', 'C',  'E', // Name + null padded
+        'N', 'A', 'M',  'E',  '1', '2', 0x00, 0x00, 0x00};
+
+    Stream stream{data};
+
+    PCEIdentity pce{stream};
+
+    EXPECT_EQ(pce.flattenedSize(), data.size());
+    EXPECT_EQ(pce.enclosureName(), "PCENAME12");
+    EXPECT_EQ(pce.mtms().machineTypeAndModel(), "TTTT-MMM");
+    EXPECT_EQ(pce.mtms().machineSerialNumber(), "123456789ABC");
+
+    // Flatten it
+    std::vector<uint8_t> newData;
+    Stream newStream{newData};
+    pce.flatten(newStream);
+
+    EXPECT_EQ(data, newData);
+}
+
+TEST(PCEIdentityTest, TestBadData)
+{
+    std::vector<uint8_t> data{
+        'P', 'E', 0x20, 0x00, 'T', 'T', 'T', 'T', '-',
+    };
+
+    Stream stream{data};
+    EXPECT_THROW(PCEIdentity pce{stream}, std::out_of_range);
+}
