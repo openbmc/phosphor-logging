318484f179b2d2b3432ca98b7336260f0a471b68
Aatir <aatrapps@gmail.com>
2019-10-18 09:37:45 -0500

    PELTool: CLI tool for reading PELs
    PEL sections need to be parsed and printed in different formats. Using
    the CLI would be one way of seeing PELs, printed in human readable
    formats.
    This class would grow as different print format for PELs are enabled and
    decided upon.
    
    Signed-off-by: Aatir <aatrapps@gmail.com>
    Change-Id: Ibc1de789eee989908e4c6fdac6a724690a67b35c

---
+++ extensions/openpower-pels/tools/peltool.hpp
@@ -0,0 +1,58 @@
+#pragma once 
+ 
+#include "../bcd_time.hpp" 
+#include "../paths.hpp" 
+#include "../pel.hpp" 
+#include "../private_header.hpp" 
+#include "../repository.hpp" 
+#include "../section.hpp" 
+#include "../stream.hpp" 
+#include "../user_header.hpp" 
+#include "pelparser.hpp" 
+#include "peltool_utility.hpp" 
+ 
+#include <bitset> 
+#include <iostream> 
+#include <sstream> 
+#include <string> 
+ 
+using namespace std; 
+ 
+namespace openpower 
+{ 
+namespace pels 
+{ 
+ 
+/** 
+ * @class PELTool 
+ * 
+ * PELTool: command line tool for viewing PELs. 
+ */ 
+class PELTool 
+{ 
+  public: 
+    PELTool() = delete; 
+    ~PELTool() = default; 
+    PELTool(const PELTool&) = default; 
+    PELTool& operator=(const PELTool&) = default; 
+    PELTool(PELTool&&) = default; 
+    PELTool& operator=(PELTool&&) = default; 
+ 
+    /** 
+     * @brief Constructor 
+     * 
+     * Fills in this class's data fields from the stream. 
+     * 
+     * @param[in] pel - the PEL data stream 
+     */ 
+    explicit PELTool(const PEL&) 
+ 
+    { 
+    } 
+ 
+  private: 
+}; 
+ 
+} // namespace pels 
+} // namespace openpower 
+
