534f911705eda5f1aab6c8b67e01908575ee5c17
Aatir <aatrapps@gmail.com>
2019-10-18 09:37:45 -0500

    PELTool: CLI tool for reading PELs
    PEL sections need to be parsed and printed in different formats. Using
    the CLI would be one way of seeing PELs, printed in human readable
    formats.
    This class would grow as different print formats for PELs are enabled and
    decided upon.
    
    Signed-off-by: Aatir <aatrapps@gmail.com>
    Change-Id: Ibc1de789eee989908e4c6fdac6a724690a67b35c

---
+++ extensions/openpower-pels/tools/peltool.cpp
@@ -0,0 +1,26 @@
+#include "peltool.hpp"
+
+#include <CLI/CLI.hpp>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <phosphor-logging/log.hpp>
+#include <sstream>
+#include <string>
+
+using namespace phosphor::logging;
+namespace fs = std::filesystem;
+using namespace openpower::pels;
+
+static void exitWithError(const std::string& help, const char* err)
+{
+    std::cerr << "ERROR: " << err << std::endl << help << std::endl;
+    exit(-1);
+}
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
