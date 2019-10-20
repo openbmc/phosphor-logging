bb148743883f51f02a6a2e7eed1e6ea74be64983
Aatir <aatrapps@gmail.com>
2019-10-20 15:13:39 -0500

    PELTool: HexDump utility
    HexDump utility function to do a hex dump for an entire PEL. This can be
    used for sections which won't have a known parsing and printing
    technique.
    
    Signed-off-by: Aatir <aatrapps@gmail.com>
    Change-Id: I155d75bb58cbd14a297b094314f7fd1f271f4f37

--- extensions/openpower-pels/tools/peltool.cpp
+++ extensions/openpower-pels/tools/peltool.cpp
@@ -1,20 +1,32 @@
-#include "peltool.hpp"
-
+#include "../pel.hpp"
 #include "peltoolutils.hpp"
 
 #include <CLI/CLI.hpp>
+#include <cstring>
+#include <fifo_map/src/fifo_map.hpp>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <iterator>
 #include <map>
+#include <nlohmann/json.hpp>
 #include <phosphor-logging/log.hpp>
 #include <sstream>
 #include <string>
+#include <vector>
+#include <xyz/openbmc_project/Common/File/error.hpp>
 
+using namespace nlohmann;
+using json = nlohmann::json;
 using namespace phosphor::logging;
 namespace fs = std::filesystem;
 using namespace openpower::pels;
+using namespace std;
+// A workaround to give to use fifo_map as map, we are just ignoring the 'less'
+// compare
+template <class K, class V, class dummy_compare, class A>
+using workaround_fifo_map = fifo_map<K, V, fifo_map_compare<K>, A>;
+using fifoMap = basic_json<workaround_fifo_map>;
 
 static void exitWithError(const std::string& help, const char* err)
 {
@@ -22,30 +34,43 @@ static void exitWithError(const std::string& help, const char* err)
     exit(-1);
 }
 
+/*PEL PELTool::getPEL()
+{
+    return _pel;
+}
+*/
+
 int main(int argc, char** argv)
 {
-    std::string pelRawFile;
-    /*    std::unique_ptr<PEL> _pel;
-        _pel = std::make_unique<PEL>(realPELData);
-        PELTool pt(*_pel);
-    */
+    // std::unique_ptr<PEL> _pel;
+    //_pel = std::make_unique<PEL>(realPELData);
     CLI::App app{"OpenBMC PEL Tool"};
     bool flagList;
     bool flagHexDump;
-    app.add_flag("--list,-l", flagList, "list all PELs in Repo");
-    app.add_flag("--hexdump, -h", flagHexDump, "Hex dump for entire PEL");
-    app.add_option(" -r, --rawpel", pelRawFile,
-                   "file for reading raw pel from");
+    app.add_flag("--list", flagList, "list all PELs in Repo");
+    app.add_flag("--hexdump", flagHexDump, "Hex dump for entire PEL");
+    std::string filename = "default";
+    app.add_option("-f,--file", filename, "Raw PEL File");
     CLI11_PARSE(app, argc, argv);
 
     if (flagHexDump)
     {
+        // hexDump(std::data(pt._pel.data()), pt._pel.data().size());
     }
-    if (!flagList && !flagHexDump)
+    if (!filename.empty())
+    {
+        std::ifstream file(filename, std::ifstream::in);
+        std::vector<uint8_t> data{std::istreambuf_iterator<char>(file),
+                                  std::istreambuf_iterator<char>()};
+        file.close();
+        PEL pel{data};
+        pel.toJson();
+    }
+    // put switch or if else elseif
+    if (!flagList && !flagHexDump && filename.empty())
     {
         exitWithError(app.help("", CLI::AppFormatMode::All),
                       "No valid id or raw pel file path not specified.");
     }
-
     return 0;
 }
---
+++ extensions/openpower-pels/tools/peltoolutils.cpp
@@ -0,0 +1,143 @@
+#include "peltoolutils.hpp"
+
+#include <stdio.h>
+
+#include <cstring>
+#include <sstream>
+#include <string>
+
+namespace openpower
+{
+namespace pels
+{
+
+char* dumpHex2(const void* data, size_t size)
+{
+    const int symbolSize = 100;
+    char* buffer = (char*)calloc(10 * size, sizeof(char));
+    char* symbol = (char*)calloc(symbolSize, sizeof(char));
+    char ascii[17];
+    size_t i, j;
+    ascii[16] = '\0';
+    for (i = 0; i < size; ++i)
+    {
+        snprintf(symbol, symbolSize, "%02X ", ((unsigned char*)data)[i]);
+        strcat(buffer, symbol);
+        memset(symbol, 0, strlen(symbol));
+        if (((unsigned char*)data)[i] >= ' ' &&
+            ((unsigned char*)data)[i] <= '~')
+        {
+            ascii[i % 16] = ((unsigned char*)data)[i];
+        }
+        else
+        {
+            ascii[i % 16] = '.';
+        }
+        if ((i + 1) % 8 == 0 || i + 1 == size)
+        {
+            strcat(buffer, " ");
+            if ((i + 1) % 16 == 0)
+            {
+                snprintf(symbol, symbolSize, "|  %s \n ", ascii);
+                strcat(buffer, symbol);
+                memset(symbol, 0, strlen(symbol));
+            }
+            else if (i + 1 == size)
+            {
+                ascii[(i + 1) % 16] = '\0';
+                if ((i + 1) % 16 <= 8)
+                {
+                    strcat(buffer, " ");
+                }
+                for (j = (i + 1) % 16; j < 16; ++j)
+                {
+                    strcat(buffer, "   ");
+                }
+                snprintf(symbol, symbolSize, "|  %s \n ", ascii);
+                strcat(buffer, symbol);
+                memset(symbol, 0, strlen(symbol));
+            }
+        }
+    }
+    free(symbol);
+    return buffer;
+}
+/**
+ * @brief Used to get the hex dump of a PEL.
+ */
+void hexDump(const void* ptr, int buflen)
+{
+    unsigned char* buf = (unsigned char*)ptr;
+    int i, j;
+    for (i = 0; i < buflen; i += 16)
+    {
+        printf("%06x: ", i);
+        for (j = 0; j < 16; j++)
+            if (i + j < buflen)
+                printf("%02x ", buf[i + j]);
+            else
+                printf("   ");
+        printf(" ");
+        for (j = 0; j < 16; j++)
+            if (i + j < buflen)
+                printf("%c", isprint(buf[i + j]) ? buf[i + j] : '.');
+        printf("\n");
+    }
+}
+
+/**
+ * @brief Used to get the hex dump of a part of a section.
+ * Implemented by derived classes.
+ */
+template <typename byte_type = std::uint8_t>
+std::string hexdump(const byte_type* buffer, std::size_t size,
+                    const std::string& title)
+{
+    using namespace std;
+    ostringstream ost;
+    ost << title << '\n';
+    const auto bytes = vector<byte_type>(buffer, buffer + size);
+    for (auto p = cbegin(bytes); p < cend(bytes);)
+    {
+        constexpr unsigned int ptrSize = sizeof(void*);
+        constexpr auto byteSize = sizeof(byte_type);
+        constexpr auto wordsPerRow = 4;
+        const auto bytesToGo =
+            static_cast<unsigned>(std::distance(p, cend(bytes)));
+        const auto nBytes = std::min(ptrSize, bytesToGo);
+        const auto bytesPrinted =
+            static_cast<unsigned>(std::distance(cbegin(bytes), p));
+        const auto isFirstRow = bytesPrinted == 0;
+        const auto isNewRow = (bytesPrinted % (ptrSize * wordsPerRow)) == 0;
+        const auto isLastRow = (p + nBytes) == cend(bytes);
+
+        if (isNewRow && !isLastRow)
+        {
+            if (!isFirstRow)
+            {
+                ost << '\n';
+            }
+            ost << hex << setw(ptrSize * 2) << setfill('0') << bytesPrinted
+                << "   ";
+        }
+
+        {
+            for_each(p, p + nBytes, [&ost](byte_type byte) {
+                ost << ' ' << hex << setw(byteSize * 2) << setfill('0')
+                    << static_cast<unsigned>(byte);
+            });
+            ost << "   ";
+        }
+
+        if (isLastRow)
+        {
+            ost << '\n';
+        }
+
+        p += nBytes;
+    }
+    return ost.str();
+}
+
+} // namespace pels
+} // namespace openpower
