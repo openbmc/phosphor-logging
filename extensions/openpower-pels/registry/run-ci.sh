239e199ec0b9138bc48c4d0c40e6abb48080220e
Matt Spinler <spinler@us.ibm.com>
2019-11-08 09:04:53 -0600

    PEL: Validate the message registry during CI
    CI will run any files called run-ci.sh it finds in a repo, so
    add a script of that name to run process_registry.py against
    the message registry to check it for errors.
    
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>
    Change-Id: I35da7c1454696b90ec88b3545992414160ae920b

---
+++ extensions/openpower-pels/registry/run-ci.sh
@@ -0,0 +1,3 @@
+/usr/bin/env python extensions/openpower-pels/registry/tools/process_registry.py \
+-v -s extensions/openpower-pels/registry/schema/schema.json \
+-r extensions/openpower-pels/registry/message_registry.json
