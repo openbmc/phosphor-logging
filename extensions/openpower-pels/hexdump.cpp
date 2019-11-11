afcbc00e8fa14c446bd35359d6cbfe39017f62dc
Aatir <aatrapps@gmail.com>
2019-11-11 13:13:42 -0600

    PEL: Hex dump bug fix
    Hex dump function had a bug where the last line was getting printed
    incorrectly.
    
    Signed-off-by: Aatir <aatrapps@gmail.com>
    Change-Id: I6acab7b57baf376b66b461c31f91a07377a7354a

--- extensions/openpower-pels/hexdump.cpp
+++ extensions/openpower-pels/hexdump.cpp
@@ -123,6 +123,9 @@ char* dumpHex(const void* data, size_t size)
                 {
                     strcat(buffer, "   ");
                 }
+                std::string asciiString2(ascii);
+                asciiString = escapeJSON(asciiString);
+                asciiToPrint = asciiString2.c_str();
                 snprintf(symbol, symbolSize, "|  %s\" \n ", asciiToPrint);
                 strcat(buffer, symbol);
                 memset(symbol, 0, strlen(symbol));
