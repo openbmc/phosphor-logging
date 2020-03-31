596b325e5888c12e2e6551ab53032d86deeb76be
Patrick Williams <patrick@stwcx.xyz>
2020-03-31 10:45:10 -0500

    build: minor python3 updates
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: Idbc9d0b2ff60fad2fff9d013ff19a4208f4d8161

--- extensions/openpower-pels/registry/run-ci.sh
+++ extensions/openpower-pels/registry/run-ci.sh
@@ -1,3 +1,3 @@
-/usr/bin/env python extensions/openpower-pels/registry/tools/process_registry.py \
+/usr/bin/env python3 extensions/openpower-pels/registry/tools/process_registry.py \
 -v -s extensions/openpower-pels/registry/schema/schema.json \
 -r extensions/openpower-pels/registry/message_registry.json
