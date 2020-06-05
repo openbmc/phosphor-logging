b181d9d141f3ad9e985ac622a665bda4e29cc30f
Matt Spinler <spinler@us.ibm.com>
2020-06-05 14:49:59 -0500

    Fixes to get unit tests to compile
    There are 2 CI failures that started showing up at the same time:
    
    1) sdbusplus recenty had a commit that started checking the return code
       on sd_bus_add_object_vtable and now 2 tests are getting those fails
       in CI, so used the mocked bus objects instead.
    
    2) pel_values_test.cpp fails with:
        Expected equality of these values:
           "cec_service_network"
             Which is: 0x563efae570fc
           std::get<registryNamePos>(*s)
             Which is: 0x563efae57a64
       because it is using EXPECT_EQ to compare to char* values instead of
       EXPECT_STREQ.  I'm not sure what changed that uncovered this.
    
    Fix them both at the same time as CI won't pass when doing them
    separately because of the other error.
    
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>
    Change-Id: I937bd6d6965be5c8aab26f6c539cf30537439e0b

--- test/openpower-pels/pel_values_test.cpp
+++ test/openpower-pels/pel_values_test.cpp
@@ -24,18 +24,18 @@ TEST(PELFieldsTest, TestFindFields)
 {
     auto s = findByValue(0x5D, subsystemValues);
     ASSERT_NE(s, subsystemValues.end());
-    ASSERT_EQ(0x5D, std::get<fieldValuePos>(*s));
-    ASSERT_EQ("cec_service_network", std::get<registryNamePos>(*s));
+    EXPECT_EQ(0x5D, std::get<fieldValuePos>(*s));
+    EXPECT_STREQ("cec_service_network", std::get<registryNamePos>(*s));
 
     s = findByName("cec_clocks", subsystemValues);
     ASSERT_NE(s, subsystemValues.end());
-    ASSERT_EQ(0x58, std::get<fieldValuePos>(*s));
-    ASSERT_EQ("cec_clocks", std::get<registryNamePos>(*s));
-    ASSERT_EQ("CEC Hardware: Clock", std::get<descriptionPos>(*s));
+    EXPECT_EQ(0x58, std::get<fieldValuePos>(*s));
+    EXPECT_STREQ("cec_clocks", std::get<registryNamePos>(*s));
+    EXPECT_STREQ("CEC Hardware: Clock", std::get<descriptionPos>(*s));
 
     s = findByValue(0xFF, subsystemValues);
-    ASSERT_EQ(s, subsystemValues.end());
+    EXPECT_EQ(s, subsystemValues.end());
 
     s = findByName("foo", subsystemValues);
-    ASSERT_EQ(s, subsystemValues.end());
+    EXPECT_EQ(s, subsystemValues.end());
 }
