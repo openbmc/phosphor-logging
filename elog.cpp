79283ff302c150a75815be1197ede640df7d4a6b
Matt Spinler <spinler@us.ibm.com>
2020-07-15 13:20:44 -0500

    LOGGING_DBUS: string constant added
    GetObject  method was removed so a fixed call to logging was added.
    Corresponding variable (host) that pulls the name was also removed.
    Since the phosphor-logging client was just calling into the
    phosphor-logging server the service name lookup was not needed.
    
    Signed-off-by: Miguel Gomez <mgomez@mx1.ibm.com>
    Change-Id: I8256bf6c3b884ef0c322df791b5a265d253fc898

--- elog.cpp
+++ elog.cpp
@@ -14,36 +14,15 @@ using namespace sdbusplus::xyz::openbmc_project::Logging::server;
 auto _prepareMsg(const char* funcName)
 {
     using phosphor::logging::log;
-    constexpr auto MAPPER_BUSNAME = "xyz.openbmc_project.ObjectMapper";
-    constexpr auto MAPPER_PATH = "/xyz/openbmc_project/object_mapper";
-    constexpr auto MAPPER_INTERFACE = "xyz.openbmc_project.ObjectMapper";
-
     constexpr auto IFACE_INTERNAL(
         "xyz.openbmc_project.Logging.Internal.Manager");
 
     // Transaction id is located at the end of the string separated by a period.
 
     auto b = sdbusplus::bus::new_default();
-    auto mapper = b.new_method_call(MAPPER_BUSNAME, MAPPER_PATH,
-                                    MAPPER_INTERFACE, "GetObject");
-    mapper.append(OBJ_INTERNAL, std::vector<std::string>({IFACE_INTERNAL}));
 
-    auto mapperResponseMsg = b.call(mapper);
-    if (mapperResponseMsg.is_method_error())
-    {
-        throw std::runtime_error("Error in mapper call");
-    }
-
-    std::map<std::string, std::vector<std::string>> mapperResponse;
-    mapperResponseMsg.read(mapperResponse);
-    if (mapperResponse.empty())
-    {
-        throw std::runtime_error("Error reading mapper response");
-    }
-
-    const auto& host = mapperResponse.cbegin()->first;
-    auto m =
-        b.new_method_call(host.c_str(), OBJ_INTERNAL, IFACE_INTERNAL, funcName);
+    auto m = b.new_method_call(BUSNAME_LOGGING, OBJ_INTERNAL, IFACE_INTERNAL,
+                               funcName);
     return m;
 }
 
