b90b8ff5f05376d26dda7c787a4ab891cd64db5a
Vijay Lobo <vijaylobo@gmail.com>
2021-06-01 16:21:02 -0500

    PEL: Support eventId property
    Support eventId property to add SRC and the hex words to the property
    
    Tested by creating the PEL log and to make sure that the eventId
    property was updated properly with 9 words and then its present in the
    right format when the logging daemon is restarted
    
    Test result:
    root@rainier# busctl get-property xyz.openbmc_project.Logging
    /xyz/openbmc_project/logging/entry/1 xyz.openbmc_project.Logging.Entry EventId
    s "BD8D1001 00000055 2E2D0010 00000000 00000000 00000000 00000000
       00000000 00000000"
    
    Change-Id: I8e39804cd3d47f0e321c1cf533b97bf165c07518
    Signed-off-by: Vijay Lobo <vijaylobo@gmail.com>

--- elog_entry.hpp
+++ elog_entry.hpp
@@ -113,6 +113,16 @@ class Entry : public EntryIfaces
 
     using sdbusplus::xyz::openbmc_project::Logging::server::Entry::resolved;
 
+    /** @brief Update enentId string of the error.
+     *  @param[in] value - Newly constructed eventId to be updated with the
+     * property
+     *  @returns New property value
+     */
+
+    std::string eventId(std::string value) override;
+
+    using sdbusplus::xyz::openbmc_project::Logging::server::Entry::eventId;
+
     /** @brief Delete this d-bus object.
      */
     void delete_() override;
