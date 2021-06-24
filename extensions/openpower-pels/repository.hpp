3cb8da0d4eaa4cc4f15248e79a8b966ccd50cb44
Ramesh Iyyar <rameshi1@in.ibm.com>
2021-06-24 05:41:51 -0500

    PEL: Repository: Added getLogID() as public method
    Currently, don't have any public method to get LogID from
    the Repository class so added an api "getLogID()" which can be
    used to get the LogID by using either OpenBMC Event ID or
    Entry ID (PEL ID).
    
    Tested:
    - Tested by below unit test cases.
     - GetLogIDFoundTC
      - With vaild Entry ID (PEL ID)
      - With vaild OpenBMC Event ID
     - GetLogIDNotFoundTC
      - With invaild Entry ID (PEL ID)
      - With invaild OpenBMC Event ID
    
    Signed-off-by: Ramesh Iyyar <rameshi1@in.ibm.com>
    Change-Id: Ia621c46f67198fccd88ec5b00c32d4f65be05743

--- extensions/openpower-pels/repository.hpp
+++ extensions/openpower-pels/repository.hpp
@@ -425,6 +425,27 @@ class Repository
         return _lastPelID;
     }
 
+    /**
+     * @brief Get the LogID based on the given ObmcID or PelID.
+     *
+     * @param[in] id - The ID to find the LogID.
+     *                 It can be either ObmcID or PelID.
+     *
+     * @return The LogID or an empty optional if not found.
+     *
+     * @note The returned LogID is the fully filled in LogID, i.e.
+     * it has both the PEL and OpenBMC IDs, unlike the passed in LogID
+     * which can just have one or the other.
+     */
+    std::optional<LogID> getLogID(const LogID& id) const
+    {
+        if (auto logID = findPEL(id); logID != _pelAttributes.end())
+        {
+            return logID->first;
+        }
+        return std::nullopt;
+    }
+
   private:
     using PELUpdateFunc = std::function<void(PEL&)>;
 
