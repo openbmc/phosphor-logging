38f683d0612c1c09ca339fddb4178e59f4ed0428
Patrick Williams <patrick@stwcx.xyz>
2021-08-02 16:31:04 -0500

    lg2: compile time header format checking
    Add support to check the format of message headers at compile
    time to verify the rules imposed by journald APIs.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: I99fcbd8918bbf11ba839843ac8e793d8402b9055

---
+++ lib/include/phosphor-logging/lg2/header.hpp
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <phosphor-logging/lg2/concepts.hpp>
+#include <string_view>
+
+namespace lg2::details
+{
+
+/** A type to handle compile-time validation of header strings. */
+struct header_str
+{
+    // Hold the header string value.
+    std::string_view value;
+
+    /** Constructor which performs validation. */
+    template <typename T>
+    consteval header_str(const T& s) : value(s)
+    {
+        if (value.size() == 0)
+        {
+            report_error(
+                "journald requires headers must have non-zero length.");
+        }
+        if (value[0] == '_')
+        {
+            report_error("journald requires header do not start with "
+                         "underscore (_)");
+        }
+
+        if (value.find_first_not_of("ABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789") !=
+            std::string_view::npos)
+        {
+            report_error(
+                "journald requires header may only contain underscore, "
+                "uppercase letters, or numbers ([_A-Z0-9]).");
+        }
+    }
+
+    /** Cast conversion back to (const char*). */
+    operator const char*() const
+    {
+        return value.data();
+    }
+
+    const char* data() const
+    {
+        return value.data();
+    }
+
+  private:
+    // This does nothing, but is useful for creating nice compile errors in
+    // a constexpr context.
+    static void report_error(const char*);
+};
+
+/** A helper type for constexpr conversion into header_str, if
+ *  'maybe_constexpr_string'.  For non-constexpr string, this does nothing.
+ */
+template <typename T>
+struct header_str_conversion
+{
+    using type = T;
+};
+
+/** Specialization for maybe_constexpr_string. */
+template <maybe_constexpr_string T>
+struct header_str_conversion<T>
+{
+    using type = const header_str&;
+};
+
+/** std-style _t alias for header_str_conversion. */
+template <typename T>
+using header_str_conversion_t = typename header_str_conversion<T>::type;
+
+} // namespace lg2::details
