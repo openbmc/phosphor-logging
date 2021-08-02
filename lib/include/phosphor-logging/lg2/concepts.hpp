14f02fe8658fe60588f491d55962f3ee2ca1185f
Patrick Williams <patrick@stwcx.xyz>
2021-08-02 16:31:04 -0500

    lg2: compile time header format checking
    Add support to check the format of message headers at compile
    time to verify the rules imposed by journald APIs.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: I99fcbd8918bbf11ba839843ac8e793d8402b9055

--- lib/include/phosphor-logging/lg2/concepts.hpp
+++ lib/include/phosphor-logging/lg2/concepts.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <concepts>
+#include <type_traits>
 
 namespace lg2::details
 {
@@ -8,5 +9,14 @@ namespace lg2::details
 /** Matches a type T which is anything except one of those in Ss. */
 template <typename T, typename... Ss>
 concept any_but = (... && !std::convertible_to<T, Ss>);
+
+/** Determine if a type might be a constexpr string: (const char (&)[N]) */
+template <typename T>
+concept maybe_constexpr_string = std::is_array_v<std::remove_cvref_t<T>> &&
+    std::same_as<const char*, std::decay_t<T>>;
+
+/** Determine if a type is certainly not a constexpr string. */
+template <typename T>
+concept not_constexpr_string = !maybe_constexpr_string<T>;
 
 }; // namespace lg2::details
