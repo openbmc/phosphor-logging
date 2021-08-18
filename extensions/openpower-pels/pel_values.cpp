b63f75b7c8975b6aa457213a7e826f794c74a171
Rashmica Gupta <rashmica.g@gmail.com>
2021-08-18 20:54:21 +1000

    PEL: Add altitude PEL registry entries
    These will only be used on systems that use PELs and have the virtual
    altitude sensor. The errors come from the sensor-monitor app.
    
    Signed-off-by: Rashmica Gupta <rashmica.g@gmail.com>
    Change-Id: I5fec3ec86a084f8c62f52194f2826c0565be32eb

--- extensions/openpower-pels/pel_values.cpp
+++ extensions/openpower-pels/pel_values.cpp
@@ -229,7 +229,7 @@ const std::map<std::string, std::string> symbolicFRUs = {
     {"usb_pgood", "USBPGD"},          {"ambient_temp", "AMBTEMP"},
     {"ambient_temp_back", "AMBBACK"}, {"ambient_perf_loss", "AMBPERF"},
     {"ac_module", "ACMODUL"},         {"fan_cable", "FANCBL"},
-    {"cable_continued", "CBLCONT"}};
+    {"cable_continued", "CBLCONT"},   {"altitude", "ALTTUDE"}};
 
 PELValues::const_iterator findByValue(uint32_t value, const PELValues& fields)
 {
