454f099ce120f11bfd41bfbaa1fdf186435a9e7c
Patrick Williams <patrick@stwcx.xyz>
2021-08-26 12:19:01 -0500

    lg2: minor size optimization in do_log
    Push the calculation of the number of logging arguments into the
    variadic 'do_log' function rather than calculate it at the lg2
    call site.  This reduces each call site by a few instructions.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: Ic8fcbdf65fb34d251d8110c5bbd05c2e20c66c18

--- lib/include/phosphor-logging/lg2/logger.hpp
+++ lib/include/phosphor-logging/lg2/logger.hpp
@@ -12,12 +12,13 @@ namespace lg2::details
  *  This is a variadic argument function (std::va_list) where the variadic
  *  arguments are a set of 3: { header, flags, value }.
  *
+ *  The argument set is finalized with a 'nullptr' valued header.
+ *
  *  @param[in] level - The logging level to use.
  *  @param[in] source_location - The original source location of the upper-level
  *                               log call.
  *  @param[in] char* - The primary message to log.
- *  @param[in] size_t - The number of sets in the variadic arguments.
  */
-void do_log(level, const std::source_location&, const char*, size_t, ...);
+void do_log(level, const std::source_location&, const char*, ...);
 
 } // namespace lg2::details
