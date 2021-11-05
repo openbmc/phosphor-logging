fdeaf4e4a8b6ad2ff7285afdbbb268cff8006e36
Jayanth Othayoth <ojayanth@in.ibm.com>
2021-11-05 04:19:43 -0500

    PEL: Added phal specific service action support
    This commits enables the basic infrastructure to add service
    actions(guard/deconfigure) supports for "phal"  feature enabled
    systems. Also enabled "guard" related service , which includes
    creating guard D-Bus object using hardware isolation D-Bus api's
    
    To enable this support, PEL users has to include the array of
    JSON callouts, which includes the below required informations.
    "Entitypath": entity path of the hardware from the PHAL device tree.
    "Guardtype": The hardware isolation severity which is defined in
                 xyz.openbmc_project.HardwareIsolation.Entry
    "Guarded": boolean, true to create gurad records.
    
    Signed-off-by: Jayanth Othayoth <ojayanth@in.ibm.com>
    Change-Id: Ibc57ae0363cd5fb9facd2ed7049a70806a5b891e

---
+++ extensions/openpower-pels/phal_service_actions.cpp
@@ -0,0 +1,91 @@
+#include "phal_service_actions.hpp"
+
+#include <attributes_info.H>
+#include <fmt/format.h>
+
+#include <phosphor-logging/log.hpp>
+
+namespace openpower
+{
+namespace pels
+{
+namespace phal
+{
+using namespace phosphor::logging;
+
+void createServiceActions(const nlohmann::json& jsonCallouts,
+                          const std::string& path,
+                          const DataInterfaceBase& dataIface)
+{
+    // Create Guard records.
+    createGuardRecords(jsonCallouts, path, dataIface);
+}
+
+void createGuardRecords(const nlohmann::json& jsonCallouts,
+                        const std::string& path,
+                        const DataInterfaceBase& dataIface)
+{
+    if (jsonCallouts.empty())
+    {
+        return;
+    }
+
+    if (!jsonCallouts.is_array())
+    {
+        log<level::ERR>("GUARD: Callout JSON isn't an array");
+        return;
+    }
+    for (const auto& _callout : jsonCallouts)
+    {
+        try
+        {
+            // Check Callout data conatains Guarded requests.
+            if (!_callout.contains("Guarded"))
+                continue;
+            if (!_callout.at("Guarded").get<bool>())
+                continue;
+            // Get Entity path required for guard D-bus method
+            // "CreateWithEntityPath"
+            if (!_callout.contains("Entitypath"))
+            {
+                log<level::ERR>(
+                    "GUARD: Callout missing, Entity path information");
+                continue;
+            }
+            using EntityPath = std::vector<uint8_t>;
+
+            auto entityPath = _callout.at("Entitypath").get<EntityPath>();
+
+            std::stringstream ss;
+            for (uint32_t a = 0; a < sizeof(ATTR_PHYS_BIN_PATH_Type); a++)
+            {
+                ss << " 0x" << std::hex << int(entityPath[a]);
+            }
+            std::string s = ss.str();
+            log<level::INFO>(fmt::format("GUARD :({})", s.c_str()).c_str());
+
+            // Get Guard type
+            auto guardType = GuardType::Warning;
+            if (!_callout.contains("Guardtype"))
+            {
+                log<level::ERR>(
+                    "GUARD: doesn't have Severity, setting to warning");
+            }
+            // Create guard record
+            auto type = sdbusplus::xyz::openbmc_project::HardwareIsolation::
+                server::convertForMessage(guardType);
+            dataIface.createGuardRecord(entityPath, type, path);
+        }
+        catch (const std::exception& e)
+        {
+            log<level::INFO>(
+                fmt::format("GUARD: Failed entry creation exception:({})",
+                            e.what())
+                    .c_str());
+        }
+    }
+}
+
+} // namespace phal
+} // namespace pels
+} // namespace openpower
