4330a05256570263a334d09c8e6b5c91d9e88672
Matt Spinler <spinler@us.ibm.com>
2022-06-08 08:25:47 -0500

    PEL: Read 'HMC Managed' BIOS attribute
    Add a property watch on the base BaseBIOSTable property provided by the
    BIOS config manager so that the pvm_hmc_managed BIOS attribute can be
    read from it to know if the HMC is managed or not.
    
    The DataInterface class already provides an isHMCManaged() method that
    is used by the host notification code, and this commit allows it to
    return the correct value.
    
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>
    Change-Id: I55e8f222361e901f0b28c3d596dc3400093aba89

--- extensions/openpower-pels/dbus_types.hpp
+++ extensions/openpower-pels/dbus_types.hpp
@@ -8,11 +8,19 @@
 namespace openpower::pels
 {
 
+using BiosAttributes =
+    std::map<std::string,
+             std::tuple<std::string, bool, std::string, std::string,
+                        std::string, std::variant<int64_t, std::string>,
+                        std::variant<int64_t, std::string>,
+                        std::vector<std::tuple<
+                            std::string, std::variant<int64_t, std::string>>>>>;
+
 using DBusValue =
     std::variant<std::string, bool, std::vector<uint8_t>,
                  std::vector<std::string>,
                  std::vector<std::tuple<std::string, std::string, std::string>>,
-                 std::tuple<uint64_t, std::vector<uint8_t>>>;
+                 std::tuple<uint64_t, std::vector<uint8_t>>, BiosAttributes>;
 using DBusProperty = std::string;
 using DBusInterface = std::string;
 using DBusService = std::string;
