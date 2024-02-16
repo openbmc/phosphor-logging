8f40e54e06f5a41054badc3c24d028f5f3ef440e
devenrao <devenrao@in.ibm.com>
2024-02-16 18:33:32 +0530

    PEL: Move definitions and variables to the proper scope
    Some of the variables and definitions defined in the header
    file are used only in the implementation file, so moving those
    to the implementation file itself.
    
    Tested:
    No testing is required only need to pass compilation.
    
    Change-Id: I4db72c12185b81c4e3e6dfa3f26d69081453c753
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

--- extensions/openpower-pels/sbe_ffdc_handler.hpp
+++ extensions/openpower-pels/sbe_ffdc_handler.hpp
@@ -44,20 +44,6 @@ constexpr uint8_t sbeFFDCSubType = 0xCB;
  *    -----------------------------------------------------------
  **/
 
-constexpr uint32_t sbeMaxFfdcPackets = 20;
-constexpr uint32_t ffdcPkgOneWord = 1;
-const uint16_t ffdcMagicCode = 0xFFDC;
-
-typedef struct
-{
-    uint32_t magic_bytes:16;
-    uint32_t lengthinWords:16;
-    uint32_t seqId:16;
-    uint32_t cmdClass:8;
-    uint32_t cmd:8;
-    uint32_t fapiRc;
-} __attribute__((packed)) fapiFfdcBufType;
-
 using LogSeverity = phosphor::logging::Entry::Level;
 
 /** @class SbeFFDC
