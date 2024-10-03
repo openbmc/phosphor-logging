openbmc/phosphor-logging

commit fa2d962b5da807ac17bb7a355709ff05dd10d041

log-manager: enable non-standard path for local tests
Enable the log-manager to be launched with a path other than
`/var/phosphor-logging` to allow it to be ran on a development system
for test purposes.  This required some refactoring throughout as to
how paths were handled.

Tested:

After running tests, `/tmp/phosphor-logging` is populated with
entries.  Launching `./builddir/phosphor-log-manager
/tmp/phosphor-logging`, the log-manager will have the entries on
dbus:

```
$ busctl --user tree xyz.openbmc_project.Logging
└─ /xyz
  └─ /xyz/openbmc_project
    └─ /xyz/openbmc_project/logging
      ├─ /xyz/openbmc_project/logging/entry
      │ ├─ /xyz/openbmc_project/logging/entry/100
      │ ├─ /xyz/openbmc_project/logging/entry/101
      │ ├─ /xyz/openbmc_project/logging/entry/102
      │ ├─ /xyz/openbmc_project/logging/entry/103
```

Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
Change-Id: I84968edeed0dcf73aaf8bb653060b6d3400b9b7b

log_manager_main.cpp
@@ -4,6 +4,7 @@
 
 #include "extensions.hpp"
 #include "log_manager.hpp"
+#include "paths.hpp"
 
 #include <phosphor-logging/lg2.hpp>
 #include <sdbusplus/bus.hpp>
@@ -12,9 +13,15 @@
 
 #include <filesystem>
 
-int main(int /*argc*/, char* /*argv*/[])
+int main(int argc, char* argv[])
 {
     PHOSPHOR_LOG2_USING_WITH_FLAGS;
+
+    if (argc >= 1)
+    {
+        PERSIST_PATH_ROOT = strdup(argv[1]);
+        info("Using temporary {PATH} for logs", "PATH", PERSIST_PATH_ROOT);
+    }
 
     auto bus = sdbusplus::bus::new_default();
     auto event = sdeventplus::Event::get_default();
@@ -28,7 +35,7 @@ int main(int /*argc*/, char* /*argv*/[])
     phosphor::logging::Manager mgr(bus, OBJ_LOGGING, iMgr);
 
     // Create a directory to persist errors.
-    std::filesystem::create_directories(ERRLOG_PERSIST_PATH);
+    std::filesystem::create_directories(phosphor::logging::paths::error());
 
     // Recreate error d-bus objects from persisted errors.
     iMgr.restore();

config/config.h.meson
@@ -25,8 +25,6 @@
 
 #define RSYSLOG_SERVER_CONFIG_FILE "@rsyslog_server_conf@"
 
-extern const char *ERRLOG_PERSIST_PATH;
-extern const char *EXTENSION_PERSIST_DIR;
 extern const bool IS_UNIT_TEST;
 
 static constexpr size_t ERROR_CAP = @error_cap@;

config_main.h
@@ -1,5 +1,4 @@
 #pragma once
 
-const char* ERRLOG_PERSIST_PATH = "/var/lib/phosphor-logging/errors";
-const char* EXTENSION_PERSIST_DIR = "/var/lib/phosphor-logging/extensions";
+const char* PERSIST_PATH_ROOT = "/var/lib/phosphor-logging";
 const bool IS_UNIT_TEST = false;

elog_entry.cpp
@@ -3,6 +3,7 @@
 #include "elog_serialize.hpp"
 #include "extensions.hpp"
 #include "log_manager.hpp"
+#include "paths.hpp"
 
 #include <fcntl.h>
 #include <unistd.h>

elog_serialize.hpp
@@ -3,6 +3,7 @@
 #include "config.h"
 
 #include "elog_entry.hpp"
+#include "paths.hpp"
 
 #include <filesystem>
 #include <string>
@@ -22,7 +23,7 @@ namespace fs = std::filesystem;
  *  @return fs::path - pathname of persisted error file
  */
 fs::path serialize(const Entry& e,
-                   const fs::path& dir = fs::path(ERRLOG_PERSIST_PATH));
+                   const fs::path& dir = fs::path(paths::error()));
 
 /** @brief Deserialze a persisted error into a d-bus object
  *  @param[in] path - pathname of persisted error file
@@ -38,8 +39,8 @@ bool deserialize(const fs::path& path, Entry& e);
  *                   be placed.
  *  @return fs::path - pathname of persisted error file
  */
-fs::path getEntrySerializePath(
-    uint32_t id, const fs::path& dir = fs::path(ERRLOG_PERSIST_PATH));
+fs::path getEntrySerializePath(uint32_t id,
+                               const fs::path& dir = fs::path(paths::error()));
 
 } // namespace logging
 } // namespace phosphor

extensions/openpower-pels/meson.build
@@ -68,6 +68,7 @@ libpel_sources = files(
     'severity.cpp',
     'user_header.cpp',
     'temporary_file.cpp',
+    '../../paths.cpp',
     '../../util.cpp',
     extra_sources,
 )
@@ -86,7 +87,7 @@ libpel_deps = [
 libpel_lib = static_library(
     'pel',
     libpel_sources,
-    'paths.cpp', # paths is separate because it is overridden during test.
+    'pel_paths.cpp', # paths is separate because it is overridden during test.
     include_directories: include_directories('../..', '../../gen'),
     cpp_args: extra_args,
     dependencies: [

extensions/openpower-pels/paths.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include "../../paths.hpp"
+
 #include <filesystem>
 
 namespace openpower

extensions/openpower-pels/pel_paths.cpp
@@ -30,14 +30,14 @@ static constexpr size_t defaultMaxNumPELs = 3000;
 
 fs::path getPELIDFile()
 {
-    fs::path logIDPath{EXTENSION_PERSIST_DIR};
+    fs::path logIDPath{phosphor::logging::paths::extension()};
     logIDPath /= fs::path{"pels"} / fs::path{"pelID"};
     return logIDPath;
 }
 
 fs::path getPELRepoPath()
 {
-    std::filesystem::path repoPath{EXTENSION_PERSIST_DIR};
+    std::filesystem::path repoPath{phosphor::logging::paths::extension()};
     repoPath /= "pels";
     return repoPath;
 }

commit 79f206b8abbbbfdec0df0f9790e6c7697aafa9f8

log-manager: fix arg bounds
An incorrect argument count check results in `argv[1]` being accessed
even when it is not provided.  This causes a SEGV on launch of the
daemon in normal cases (on a system) when the temporary path location
is not provided.

Fixes: fa2d962b5da807ac17bb7a355709ff05dd10d041
Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
Change-Id: Ibe7254533cb6002d73c5a1ada8bb1f35e9c9df53

log_manager_main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char* argv[])
 {
     PHOSPHOR_LOG2_USING_WITH_FLAGS;
 
-    if (argc >= 1)
+    if (argc >= 2)
     {
         PERSIST_PATH_ROOT = strdup(argv[1]);
         info("Using temporary {PATH} for logs", "PATH", PERSIST_PATH_ROOT);