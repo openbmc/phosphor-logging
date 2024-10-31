9ca4d137ad0f0dd03074652c55186a035fba27d1
Patrick Williams <patrick@stwcx.xyz>
2024-10-31 17:02:47 -0400

    manager: use new Logging.Cleared event
    Modify the internal manager support for creating internal events
    to be able to support the sdbusplus generated events.  Transition
    from the local Logging.Error.LogsCleared to the global Logging.Cleared
    event.  Clean up references to the old event.
    
    Tested:
    ```
    $ busctl --user call xyz.openbmc_project.Logging /xyz/openbmc_project/logging xyz.openbmc_project.Collection.DeleteAll DeleteAll
    $ busctl --user introspect xyz.openbmc_project.Logging /xyz/openbmc_project/logging/entry/1 -l | cat
    NAME                                        TYPE      SIGNATURE RESULT/VALUE                                                                                                                                             FLAGS
    xyz.openbmc_project.Logging.Entry           interface -         -                                                                                                                                                        -
    .GetEntry                                   method    -         h                                                                                                                                                        -
    .AdditionalData                             property  as        5 "NUMBER_OF_LOGS=210" "_CODE_FILE=../log_manager.hpp" "_CODE_FUNC=virtual void phosphor::logging::Manager::deleteAll()" "_CODE_LINE=361" "_PID=3318436" emits-change writable
    .EventId                                    property  s         ""                                                                                                                                                       emits-change writable
    .Id                                         property  u         1                                                                                                                                                        emits-change writable
    .Message                                    property  s         "xyz.openbmc_project.Logging.Cleared"                                                                                                                    emits-change writable
    .Resolution                                 property  s         ""                                                                                                                                                       emits-change writable
    .Resolved                                   property  b         false                                                                                                                                                    emits-change writable
    .ServiceProviderNotify                      property  s         "xyz.openbmc_project.Logging.Entry.Notify.NotSupported"                                                                                                  emits-change writable
    .Severity                                   property  s         "xyz.openbmc_project.Logging.Entry.Level.Informational"                                                                                                  emits-change writable
    .Timestamp                                  property  t         1730408674348                                                                                                                                            emits-change writable
    .UpdateTimestamp                            property  t         1730408674348                                                                                                                                            emits-change writable
    ```
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: I470f33ae2f5bfae6bf6d6a80ac2c5f028e41da95

---
+++ lib/lg2_commit.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <sdbusplus/exception.hpp>
+#include <xyz/openbmc_project/Logging/Entry/client.hpp>
+
+#include <string>
+#include <tuple>
+#include <vector>
+
+namespace lg2::details
+{
+
+using Entry = sdbusplus::client::xyz::openbmc_project::logging::Entry<>;
+
+/** Extract the message, level, and additional data from agenerated event.
+ *
+ *  @param[in] The event to extract data from.
+ *  @return A tuple containing the message, level, and additional data.
+ */
+auto extractEvent(sdbusplus::exception::generated_event_base&&)
+    -> std::tuple<std::string, Entry::Level, std::vector<std::string>>;
+
+} // namespace lg2::details
