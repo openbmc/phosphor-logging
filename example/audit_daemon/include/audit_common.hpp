a88a2f871eed94e120c3e7f339e387c0335bad51
Denis SHashunkin <schle1fer@yahoo.com>
2024-12-24 17:17:52 +0000

    Phosphor audit API and audit daemon
    This patchset is the phosphor audit system as a subproject in the openbmc/phosphor-logging project.
    Made it based on the prototype document as described by Ivan Mikhaylov here https://gerrit.openbmc.org/c/openbmc/docs/+/63915
    
    In short, the phosphor audit system consists of a client part (audit API) and a phosphor audit daemon.
    This daemon receives events from any services(IPMI,PAM,SSH etc.) that use these APIs for logging, processes them, redirects them, etc.
    
    In example, the phosphor audit daemon receives/processes/sends events to the Linux kernel auditd subsystem. It has own config file.
    
    Th phosphor audit uses the D-Bus bus for interprocess communication and transmitting an event for logging by calling the Notify method.
    
    In this discussion https://gerrit.openbmc.org/c/openbmc/phosphor-logging/+/51285 where it is written that D-Bus is a public interface and that it is not quite good.
    Yes, this is true from the point of that knowing this D-Bus interface and its methods, we can call a method on this bus(for example, from console).
    On the other hand, each application that uses phosphor audit to log its events uses its own built-in ID.
    After all, it is always possible to grep application events by PID in the journal log.
    
    Instead of D-Bus, phosphor audit system can use any other standard mechanism for interprocessor communication.
    
    lib/include/audit - audit API
    example/audit_api_usage - example for usage autit API
    example/audit_daemon - audit daemon example
    
    Change-Id: I12d082f1e2bc0a8a7db006bd1ec6b94c735a9204
    Signed-off-by: Denis SHashunkin <schle1fer@yahoo.com>

---
+++ example/audit_daemon/include/audit_common.hpp
@@ -0,0 +1,28 @@
+/**
+ * @brief Phosphor Audit common constants
+ *
+ * This file is part of the phosphor-logging audit project.
+ *
+ */
+
+#pragma once
+
+#include <string>
+#include <tuple>
+#include <vector>
+
+namespace phosphor
+{
+namespace logging
+{
+namespace audit
+{
+
+// constants
+static constexpr POWERON = 1;
+static constexpr POWEROFF = 2;
+static constexpr FACTORY_RESET = 3;
+
+} // namespace audit
+} // namespace logging
+} // namespace phosphor
