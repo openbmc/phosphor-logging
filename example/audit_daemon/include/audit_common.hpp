2d3cd86b60c012dadf8823ef260f300d297f74db
Ivan Mikhaylov <fr0st61te@gmail.com>
2025-01-14 16:24:09 +0300

    Phosphor audit API and audit daemon
    This patchset is POC of phosphor audit system as a subproject in the
    openbmc/phosphor-logging project. Made it based on the prototype
    document as described by Ivan Mikhaylov here [1].
    
    In short, the phosphor audit system consists of a client part
    (audit API) and a phosphor audit daemon. This daemon receives events
    from any services(IPMI,PAM,SSH etc.) that use these APIs for logging,
    processes them, redirects them, etc.
    
    The chain call in example with auditd:
    client -> phosphor-logging -> audit_daemon -> auditd
    
    In example, the phosphor audit daemon receives/processes/sends events
    to the Linux kernel auditd subsystem which has own config file.
    
    The phosphor audit uses the D-Bus bus for interprocess communication
    and transmitting an event for logging by calling the Notify method.
    
    In this discussion [2] where it is written that D-Bus is the public
    interface and that it is not quite good. Yes, this is true from the
    point of that knowing this D-Bus interface and its methods, we can
    call a method on this bus(for example, from console). On the other
    hand, each application that uses phosphor audit to log its events uses
    its own built-in ID. After all, it is always possible to grep
    application events by PID in the journal log.
    
    Instead of D-Bus, phosphor audit system can use any other standard
    mechanism for interprocessor communication.
    
    lib/include/audit - audit API
    example/audit_api_usage - example for usage autit API
    example/audit_daemon - audit daemon example
    
    Example:
    ```
    ./audit_api_usage --type=0 --request=1 --retcode=0 --user=root
    ```
    Result:
    journalctl -t audit -aex
    ```
    audit[662]: AUDIT1120 pid=662 uid=0 auid=4294967295 ses=4294967295 msg='Item: type[0] requestId[1] return code[0] user[root] data size[4] sockaddr size[28] exe="/tmp/phosphor-auditd" hostname=? addr=Hello terminal=? res=failed'
    ```
    
    [1] ```https://gerrit.openbmc.org/c/openbmc/docs/+/63915```
    [2] ```https://gerrit.openbmc.org/c/openbmc/phosphor-logging/+/51285```
    
    Signed-off-by: Denis SHashunkin <schle1fer@yahoo.com>
    Signed-off-by: Ivan Mikhaylov <fr0st61te@gmail.com>
    Change-Id: I12d082f1e2bc0a8a7db006bd1ec6b94c735a9204

---
+++ example/audit_daemon/include/audit_common.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+#include <tuple>
+#include <vector>
+
+namespace phosphor
+{
+namespace logging
+{
+namespace audit
+{
+
+// requestId operations
+static constexpr uint32_t POWEROP = 1;
+static constexpr uint32_t SETUSERPASSWD = 2;
+
+} // namespace audit
+} // namespace logging
+} // namespace phosphor
