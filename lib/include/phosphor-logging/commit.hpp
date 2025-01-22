63c84a6b0e0a26afcc6bfe59a3d60c6ab382ca56
Amithash Prasasd <amithash@meta.com>
2025-01-22 10:36:02 -0800

    Add helper method to resolve a log object path
    Add a helper method to resolve a previously generated
    log entry which can be used by other services in conjunction
    with the `lg2::commit` API.
    
    Change-Id: Ia1582177b51ca2f0bfecae98f999b5b8f2f61c93
    Signed-off-by: Amithash Prasasd <amithash@meta.com>

--- lib/include/phosphor-logging/commit.hpp
+++ lib/include/phosphor-logging/commit.hpp
@@ -16,6 +16,16 @@ namespace lg2
 auto commit(sdbusplus::exception::generated_event_base&& e)
     -> sdbusplus::message::object_path;
 
+/** Resolve an existing event/error.
+ *
+ *  @param logPath - The object path of the event to resolve.
+ *  @return None.
+ *
+ *  Note: Similar to elog(), this will use the default dbus connection to
+ *  perform the operation.
+ */
+void resolve(const sdbusplus::message::object_path& logPath);
+
 /** Commit a generated event/error (using async context).
  *
  *  @param ctx - The async context to use.
@@ -25,4 +35,15 @@ auto commit(sdbusplus::exception::generated_event_base&& e)
 auto commit(sdbusplus::async::context& ctx,
             sdbusplus::exception::generated_event_base&& e)
     -> sdbusplus::async::task<sdbusplus::message::object_path>;
+
+/** Resolve an existing event/error (using async context).
+ *
+ *  @param ctx - The async context to use.
+ *  @param logPath - The object path of the event to resolve.
+ *  @return None
+ */
+auto resolve(sdbusplus::async::context& ctx,
+             const sdbusplus::message::object_path& logPath)
+    -> sdbusplus::async::task<>;
+
 } // namespace lg2
