433f61df7a39ea2a99cd2966d0c840bd8f0295e0
Matt Spinler <spinler@us.ibm.com>
2025-05-09 11:39:08 -0500

    clang-tidy: Delete unused copy/move ctor/operator
    Explicitly delete the copy assignment, move assignment, and move
    constructors on the classes where they are unused.
    
    Change-Id: I73298debb4e3e894af5a02006b87d26b70f4535d
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

--- extensions/openpower-pels/pldm_interface.hpp
+++ extensions/openpower-pels/pldm_interface.hpp
@@ -29,10 +29,10 @@ class PLDMInterface : public HostInterface
 {
   public:
     PLDMInterface() = delete;
-    PLDMInterface(const PLDMInterface&) = default;
-    PLDMInterface& operator=(const PLDMInterface&) = default;
-    PLDMInterface(PLDMInterface&&) = default;
-    PLDMInterface& operator=(PLDMInterface&&) = default;
+    PLDMInterface(const PLDMInterface&) = delete;
+    PLDMInterface& operator=(const PLDMInterface&) = delete;
+    PLDMInterface(PLDMInterface&&) = delete;
+    PLDMInterface& operator=(PLDMInterface&&) = delete;
 
     /**
      * @brief Constructor
