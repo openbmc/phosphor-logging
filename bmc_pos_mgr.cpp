124d5da765788ea960e29303ba69b896ac9d7886
Matt Spinler <spinler@us.ibm.com>
2025-10-28 15:18:45 -0500

    Store BMC pos in entry ID if option enabled
    This is for use in multiple (most likely redundant) BMC systems to
    produce unique event log IDs across all the BMCs.  It does this by
    storing the BMC position in the upper byte of the event log ID.  If the
    position can't be obtained, it will encode 0xFF instead.
    
    It is enabled when the new 'use-bmc-pos-in-id' meson option is set to
    true.
    
    There is a new BMCPosMgr class to handle this to prevent adding more
    complexity into log_manager.cpp and to make it easier to test.
    
    Additional details:
    * On startup, read the BMC position on the Decorator.Position interface
      from the 'bmc-position-object-path' object path.
    * If the position isn't on D-Bus, or if the value is maxint, consider
      the position to be 0xFF.
    * Store the BMC position in the upper byte of the event log ID, making
      sure to then roll it over at 0x00FFFFFF instead of 0xFFFFFFFF.
    * On new logs, if the position isn't known, try to read it again in case
      the problem preventing it was resolved (e.g. a cable was plugged in).
    
    On startup, or when the position goes from unknown to known, delete any
    existing logs that have the 'no-position' (0xFF) value in them.  Since
    the 0xFF isn't unique across BMCs it would be possible that a log with
    the same ID could be created by multiple BMCs and if copied (i.e.
    data-synced) to other BMCs they could overwrite each other.
    
    Tested:
    On BMC 1, where 16777217 = 0x01000001
    ```
    $ busctl tree xyz.openbmc_project.Logging
    └─ /xyz
      └─ /xyz/openbmc_project
        └─ /xyz/openbmc_project/logging
          ├─ /xyz/openbmc_project/logging/entry
          │ └─ /xyz/openbmc_project/logging/entry/16777217
    ```
    
    And the file:
    ```
    /var/lib/phosphor-logging/errors# ls
    16777217
    ```
    
    On BMC 0:
    ```
    ...
          │ ├─ /xyz/openbmc_project/logging/entry/1
    ```
    
    And the file:
    ```
    /var/lib/phosphor-logging/errors# ls
    1
    ```
    
    With no position, where 4278190081 = 0xFF000001
    ```
    $ busctl tree xyz.openbmc_project.Logging
    └─ /xyz
      └─ /xyz/openbmc_project
        └─ /xyz/openbmc_project/logging
          ├─ /xyz/openbmc_project/logging/entry
          │ ├─ /xyz/openbmc_project/logging/entry/4278190081
          │ └─ /xyz/openbmc_project/logging/entry/4278190082
    
    ```
    and
    ```
    /var/lib/phosphor-logging/errors# ls
    4278190081 4278190082
    ```
    
    And when a position is obtained after logs were created already:
    ```
    phosphor-log-manager[3210]: Found BMC position 0
    phosphor-log-manager[3210]: Removing log 4278190081 that didn't have a position because now there is one
    phosphor-log-manager[3210]: Removing log 4278190082 that didn't have a position because now there is one
    ```
    
    Change-Id: Ib0677e039acf135fbe811a300b64e1fca140ea75
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

---
+++ bmc_pos_mgr.cpp
@@ -0,0 +1,122 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+
+#include "config.h"
+
+#include "bmc_pos_mgr.hpp"
+
+#include "log_manager.hpp"
+#include "util.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+#include <xyz/openbmc_project/Inventory/Decorator/Position/client.hpp>
+
+namespace phosphor::logging
+{
+
+constexpr uint32_t noPosition = 0xFF;
+constexpr uint32_t entryIdValueMask = 0x00FFFFFF;
+
+void BMCPosMgr::init(sdbusplus::bus_t& bus)
+{
+    setBMCPosition(getBMCPosition(bus));
+}
+
+void BMCPosMgr::setBMCPosition(const std::optional<size_t>& bmcPosFromDBus)
+{
+    if (!bmcPosFromDBus.has_value())
+    {
+        // Not on D-Bus
+        lg2::error("Could not get BMC position");
+        bmcPosition = std::nullopt;
+    }
+    else if (bmcPosFromDBus.value() == std::numeric_limits<size_t>::max())
+    {
+        // On D-Bus, but position can't be obtained from the HW
+        lg2::warning("BMC position value could not be obtained from hardware");
+        bmcPosition = std::nullopt;
+    }
+    else
+    {
+        auto pos = static_cast<uint32_t>(bmcPosFromDBus.value());
+        lg2::info("Found BMC position {POSITION}", "POSITION", pos);
+        bmcPosition = pos;
+    }
+}
+
+uint32_t BMCPosMgr::processEntryId(uint32_t id)
+{
+    setPrefixInEntryId(id);
+
+    // Roll it over at 0x00FFFFFF
+    checkEntryIdRollover(id);
+
+    if (idHasNoPosition(id))
+    {
+        idsWithNoPosition.push_back(id + 1);
+    }
+
+    return id;
+}
+
+void BMCPosMgr::setPrefixInEntryId(uint32_t& id)
+{
+    uint32_t posField = bmcPosition.value_or(noPosition);
+    uint32_t prefix = (posField & 0xFF) << 24;
+
+    id &= entryIdValueMask;
+    id |= prefix;
+}
+
+bool BMCPosMgr::idHasNoPosition(uint32_t id)
+{
+    return (id & ~entryIdValueMask) == (noPosition << 24);
+}
+
+void BMCPosMgr::checkEntryIdRollover(uint32_t& id)
+{
+    if ((id & entryIdValueMask) == entryIdValueMask)
+    {
+        lg2::info("Event log ID rolled over");
+        id &= ~entryIdValueMask;
+    }
+}
+
+std::optional<size_t> BMCPosMgr::getBMCPosition(sdbusplus::bus_t& bus)
+{
+    using Position = sdbusplus::client::xyz::openbmc_project::inventory::
+        decorator::Position<>;
+
+    try
+    {
+        auto service =
+            util::getService(bus, BMC_POS_OBJECT_PATH, Position::interface);
+
+        return util::getProperty<size_t>(bus, service, BMC_POS_OBJECT_PATH,
+                                         Position::interface, "Position");
+    }
+    catch (const std::exception& e)
+    {
+        lg2::error("Unable to get BMC position: {ERROR}", "ERROR", e);
+    }
+
+    return std::nullopt;
+}
+
+void BMCPosMgr::removeNoPosLogs(internal::Manager& logMgr)
+{
+    // If the position is now known, remove any logs created
+    // when there wasn't one.
+    if (!idsWithNoPosition.empty() && bmcPosition.has_value())
+    {
+        std::ranges::for_each(idsWithNoPosition, [&logMgr](auto id) {
+            lg2::info(
+                "Removing log {ID} that didn't have a position because now there is one",
+                "ID", id);
+            logMgr.erase(id);
+        });
+        idsWithNoPosition.clear();
+    }
+}
+
+} // namespace phosphor::logging
---
+++ bmc_pos_mgr.hpp
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+#pragma once
+
+#include <sdbusplus/bus.hpp>
+
+#include <cstdint>
+#include <optional>
+
+namespace phosphor::logging
+{
+
+namespace internal
+{
+class Manager;
+};
+
+/**
+ * @class BMCPosMgr
+ *
+ * Manages encoding the BMC position into the upper byte
+ * of the event log ID.  If the position isn't there, it will
+ * use 0xFF instead.
+ *
+ */
+class BMCPosMgr
+{
+  public:
+    BMCPosMgr() = default;
+    ~BMCPosMgr() = default;
+    BMCPosMgr(const BMCPosMgr&) = delete;
+    BMCPosMgr& operator=(const BMCPosMgr&) = delete;
+    BMCPosMgr(BMCPosMgr&&) = delete;
+    BMCPosMgr& operator=(BMCPosMgr&&) = delete;
+
+    /**
+     * @brief The BMC position.  std::nullopt if it isn't on
+     *        D-Bus or is the magic value that means it isn't
+     *        possible to ascertain for some reason.
+     */
+    std::optional<uint32_t> bmcPosition;
+
+    /**
+     * @brief Gets the BMC position from D-Bus
+     *
+     * @param[in] bus - the sdbusplus bus
+     */
+    void init(sdbusplus::bus_t& bus);
+
+    /**
+     * @brief Sets the bmcPosition member var from
+     *        the D-Bus property value.
+     *
+     * A D-Bus value of std::numeric_limits<size_t>::max() translates
+     * to a bmcPosition value of std::nullopt;
+     *
+     * @param[in] bmcPosFromDBus - The D-Bus property value
+     */
+    void setBMCPosition(const std::optional<size_t>& bmcPosFromDBus);
+
+    /**
+     * @brief Says if the ID passed in has the 'no position' value
+     *        encoded into it.
+     *
+     * @param[in] id - The entry ID
+     *
+     * @return true - If the ID contains 'no position'
+     */
+    bool idHasNoPosition(uint32_t id);
+
+    /**
+     * @brief Updates entryID with the current BMC position value
+     *        and returns it.
+     *
+     * Adds the ID to idsWithNoPosition if there is no position.
+     * Rolls over the ID if necessary.
+     *
+     * @param[in] id - The entry ID
+     *
+     * @return The updated entry ID
+     */
+    uint32_t processEntryId(uint32_t id);
+
+    /**
+     * @brief Deletes all event logs that have IDs in idsWithNoPosition
+     *        if there is now a position.
+     *
+     * @param[in] logMgr - The Manager object that can erase event logs
+     */
+    void removeNoPosLogs(internal::Manager& logMgr);
+
+    /**
+     * @brief Adds an entry ID with no position to the list
+     *
+     * @param[in] id - The ID to add
+     */
+    inline void addNoPosEntryId(uint32_t id)
+    {
+        idsWithNoPosition.push_back(id);
+    }
+
+  private:
+    /**
+     * @brief Sets the position prefix in the entry ID
+     *
+     * @param[inout] id - The entry ID
+     */
+    void setPrefixInEntryId(uint32_t& id);
+
+    /**
+     * @brief Reads the BMC position property from D-Bus
+     *
+     * @param[in] bus - the sdbusplus bus
+     * @return The position property value. std::nullopt if not on D-Bus
+     */
+    std::optional<size_t> getBMCPosition(sdbusplus::bus_t& bus);
+
+    /**
+     * @brief If the ID passed in is the last one before a
+     *        rollover, do the rollover.
+     *
+     *        e.g. 0x00FFFFFF will roll over to 0x00000000
+     *
+     * @param[inout] The ID that may be rolled over
+     */
+    void checkEntryIdRollover(uint32_t& id);
+
+    /**
+     * @brief All event log IDs that have 'no position' in them.
+     */
+    std::vector<uint32_t> idsWithNoPosition;
+};
+
+} // namespace phosphor::logging
