14f816f1c5ac745723204ffd983bd850516ca896
Matt Spinler <spinler@us.ibm.com>
2025-10-28 15:18:45 -0500

    Store BMC pos in entry ID if option enabled
    This is for use in multiple (most likely redundant) BMC systems to
    produce unique event log IDs across all the BMCs.  It does this by
    storing the BMC position in the upper byte of the event log ID.  If the
    position can't be obtained, it will encode 0xFF instead.
    
    It is enabled when the new 'use-bmc-pos-in-id' meson option is set to
    true.
    
    There is a new BMCPosMgr class to handle this to prevent adding more
    complexity into log_manager.cpp and to make it easier to test.
    
    Additional details:
    * On startup, read the BMC position from /run/openbmc/bmc_position.
    * If file doesn't exist, or if the value is maxint, consider the
      position to be 0xFF.
    * Store the BMC position in the upper byte of the event log ID, making
      sure to then roll it over at 0x00FFFFFF instead of 0xFFFFFFFF.
    * On new logs, if the position isn't known, try to read it again in case
      the problem preventing it was resolved (e.g. a cable was plugged in).
    
    Tested:
    On BMC 1, where 16777217 = 0x01000001
    ```
    $ busctl tree xyz.openbmc_project.Logging
    └─ /xyz
      └─ /xyz/openbmc_project
        └─ /xyz/openbmc_project/logging
          ├─ /xyz/openbmc_project/logging/entry
          │ └─ /xyz/openbmc_project/logging/entry/16777217
    ```
    
    And the file:
    ```
    /var/lib/phosphor-logging/errors# ls
    16777217
    ```
    
    On BMC 0:
    ```
    ...
          │ ├─ /xyz/openbmc_project/logging/entry/1
    ```
    
    And the file:
    ```
    /var/lib/phosphor-logging/errors# ls
    1
    ```
    
    With no position, where 4278190081 = 0xFF000001
    ```
    $ busctl tree xyz.openbmc_project.Logging
    └─ /xyz
      └─ /xyz/openbmc_project
        └─ /xyz/openbmc_project/logging
          ├─ /xyz/openbmc_project/logging/entry
          │ ├─ /xyz/openbmc_project/logging/entry/4278190081
          │ └─ /xyz/openbmc_project/logging/entry/4278190082
    
    ```
    and
    ```
    /var/lib/phosphor-logging/errors# ls
    4278190081 4278190082
    ```
    
    Change-Id: Ib0677e039acf135fbe811a300b64e1fca140ea75
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

--- log_manager.hpp
+++ log_manager.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "config.h"
+
+#include "bmc_pos_mgr.hpp"
 #include "elog_block.hpp"
 #include "elog_entry.hpp"
 #include "xyz/openbmc_project/Logging/Internal/Manager/server.hpp"
@@ -72,7 +75,13 @@ class Manager : public details::ServerObject<details::ManagerIface>
      */
     Manager(sdbusplus::bus_t& bus, const char* objPath) :
         details::ServerObject<details::ManagerIface>(bus, objPath), busLog(bus),
-        entryId(0), fwVersion(readFWVersion()) {};
+        entryId(0), fwVersion(readFWVersion())
+    {
+        if constexpr (USE_BMC_POS_IN_ID)
+        {
+            bmcPosMgr = std::make_unique<BMCPosMgr>();
+        }
+    };
 
     /*
      * @fn commit()
@@ -315,6 +324,9 @@ class Manager : public details::ServerObject<details::ManagerIface>
     /** @brief Map of entry id to call back object on properties changed */
     std::map<uint32_t, std::unique_ptr<sdbusplus::bus::match_t>>
         propChangedEntryCallback;
+
+    /** @brief Encodes the BMC position in the entryId when enabled */
+    std::unique_ptr<BMCPosMgr> bmcPosMgr;
 };
 
 } // namespace internal
