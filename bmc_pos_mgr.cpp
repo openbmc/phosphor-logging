b6c6e2dbab1f4c903bf045b927d7f92bb85f8670
Matt Spinler <spinler@us.ibm.com>
2025-10-28 15:18:45 -0500

    Store BMC pos in entry ID if option enabled
    This is for use in multiple (most likely redundant) BMC systems to
    produce unique event log IDs across all the BMCs.  It does this by
    storing the BMC position in the upper byte of the event log ID.  If the
    position can't be obtained, it will encode 0xFF instead.
    
    It is enabled when the new 'use-bmc-pos-in-id' meson option is set to
    true.
    
    There is a new BMCPosMgr class to handle this to prevent adding more
    complexity into log_manager.cpp and to make it easier to test.
    
    Additional details:
    * On startup, read the BMC position from /run/openbmc/bmc_position.
    * If file doesn't exist, or if the value is maxint, consider the
      position to be 0xFF.
    * Store the BMC position in the upper byte of the event log ID, making
      sure to then roll it over at 0x00FFFFFF instead of 0xFFFFFFFF.
    * On new logs, if the position isn't known, try to read it again in case
      the problem preventing it was resolved (e.g. a cable was plugged in).
    
    Tested:
    On BMC 1, where 16777217 = 0x01000001
    ```
    $ busctl tree xyz.openbmc_project.Logging
    └─ /xyz
      └─ /xyz/openbmc_project
        └─ /xyz/openbmc_project/logging
          ├─ /xyz/openbmc_project/logging/entry
          │ └─ /xyz/openbmc_project/logging/entry/16777217
    ```
    
    And the file:
    ```
    /var/lib/phosphor-logging/errors# ls
    16777217
    ```
    
    On BMC 0:
    ```
    ...
          │ ├─ /xyz/openbmc_project/logging/entry/1
    ```
    
    And the file:
    ```
    /var/lib/phosphor-logging/errors# ls
    1
    ```
    
    With no position, where 4278190081 = 0xFF000001
    ```
    $ busctl tree xyz.openbmc_project.Logging
    └─ /xyz
      └─ /xyz/openbmc_project
        └─ /xyz/openbmc_project/logging
          ├─ /xyz/openbmc_project/logging/entry
          │ ├─ /xyz/openbmc_project/logging/entry/4278190081
          │ └─ /xyz/openbmc_project/logging/entry/4278190082
    
    ```
    and
    ```
    /var/lib/phosphor-logging/errors# ls
    4278190081 4278190082
    ```
    
    Change-Id: Ib0677e039acf135fbe811a300b64e1fca140ea75
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

---
+++ bmc_pos_mgr.cpp
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+
+#include "bmc_pos_mgr.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+
+#include <fstream>
+
+namespace phosphor::logging
+{
+
+constexpr uint8_t noPosition = 0xFF;
+constexpr uint32_t entryIdValueMask = 0x00FFFFFF;
+
+uint32_t BMCPosMgr::processEntryId(uint32_t id) const
+{
+    setPrefixInEntryId(id);
+
+    // Roll it over at 0x00FFFFFF
+    checkEntryIdRollover(id);
+
+    return id;
+}
+
+bool BMCPosMgr::isPositionValid() const
+{
+    return bmcPosition != noPosition;
+}
+
+void BMCPosMgr::setPrefixInEntryId(uint32_t& id) const
+{
+    uint32_t prefix = static_cast<uint32_t>(bmcPosition) << 24;
+
+    id &= entryIdValueMask;
+    id |= prefix;
+}
+
+void BMCPosMgr::checkEntryIdRollover(uint32_t& id) const
+{
+    if ((id & entryIdValueMask) == entryIdValueMask)
+    {
+        lg2::info("Event log ID rolled over");
+        id &= ~entryIdValueMask;
+    }
+}
+
+void BMCPosMgr::readBMCPosition()
+{
+    bmcPosition = noPosition;
+
+    std::error_code ec;
+    if (!std::filesystem::exists(posFile, ec))
+    {
+        lg2::error("BMC position file {FILE} doesn't exist", "FILE", posFile);
+        return;
+    }
+
+    std::ifstream stream{posFile};
+    if (!stream)
+    {
+        lg2::error("Could not open BMC position file {FILE}", "FILE", posFile);
+        return;
+    }
+
+    size_t position;
+    stream >> position;
+
+    if (stream.fail())
+    {
+        lg2::error("Failed reading BMC position out of {FILE}", "FILE",
+                   posFile);
+        return;
+    }
+
+    if (position == std::numeric_limits<size_t>::max())
+    {
+        lg2::error("BMC position value could not be obtained from hardware");
+        return;
+    }
+
+    bmcPosition = static_cast<uint8_t>(position);
+    lg2::info("Found BMC position {POSITION}", "POSITION", bmcPosition);
+}
+
+} // namespace phosphor::logging
---
+++ test/bmc_pos_mgr_test.cpp
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+
+#include "bmc_pos_mgr.hpp"
+
+#include <fstream>
+
+#include <gtest/gtest.h>
+
+using namespace phosphor::logging;
+
+class PosFile
+{
+  public:
+    PosFile(size_t pos)
+    {
+        auto fd = mkstemp(path.data());
+        if (fd == -1)
+        {
+            throw std::runtime_error{
+                std::string{"Unable to create temporary file: "} +
+                strerror(errno)};
+        }
+        close(fd);
+        write(pos);
+    }
+
+    ~PosFile()
+    {
+        remove();
+    }
+
+    void write(size_t pos)
+    {
+        std::ofstream stream{path};
+        if (!stream)
+        {
+            throw std::runtime_error("Could not open " + path);
+        }
+        stream << pos;
+    }
+
+    void remove()
+    {
+        std::error_code ec;
+        std::filesystem::remove(path);
+    }
+
+    std::string path{std::filesystem::temp_directory_path() / "bmc-pos-XXXXXX"};
+};
+
+TEST(BMCPosMgrTest, ReadBMCPosition)
+{
+    PosFile file{0};
+    BMCPosMgr mgr{file.path};
+
+    EXPECT_EQ(mgr.getBMCPosition(), 0);
+
+    file.write(1);
+    mgr.readBMCPosition();
+    EXPECT_EQ(mgr.getBMCPosition(), 1);
+
+    file.write(0xFF);
+    mgr.readBMCPosition();
+    EXPECT_EQ(mgr.getBMCPosition(), 0xFF);
+
+    file.write(std::numeric_limits<size_t>::max());
+    mgr.readBMCPosition();
+    EXPECT_EQ(mgr.getBMCPosition(), 0xFF);
+
+    file.remove();
+    mgr.readBMCPosition();
+    EXPECT_EQ(mgr.getBMCPosition(), 0xFF);
+}
+
+TEST(BMCPosMgrTest, ProcessEntryId)
+{
+    struct TestEntry
+    {
+        uint8_t pos;       // Position
+        uint32_t id;       // Current entry ID
+        uint32_t expected; // EntryID value after processEntryId()
+    };
+
+    const std::vector<TestEntry> tests = {
+        // No position change no rollover
+        {0, 1, 1},
+        {1, 1, 0x01000001},
+        {0xFF, 0xFF000000, 0xFF000000},
+
+        // Change positions
+        {0, 0x01000001, 0x00000001},
+        {1, 0x00000001, 0x01000001},
+        {0xFF, 0x00000001, 0xFF000001},
+        {0, 0xFF000001, 0x00000001},
+
+        // Rollovers
+        {0xFF, 0xFFFFFFFF, 0xFF000000},
+        {0, 0x00FFFFFF, 0x00000000},
+        {1, 0x01FFFFFF, 0x01000000},
+        {1, 0xFFFFFFFF, 0x01000000}};
+
+    for (const auto& test : tests)
+    {
+        PosFile file{test.pos};
+        BMCPosMgr mgr{file.path};
+
+        EXPECT_EQ(mgr.processEntryId(test.id), test.expected);
+    }
+}
