3b1084d6614ae334ef6333aae7290d0929cd1bfe
Matt Spinler <spinler@us.ibm.com>
2025-10-28 15:18:45 -0500

    Store BMC pos in entry ID if option enabled
    This is for use in multiple (most likely redundant) BMC systems to
    produce unique event log IDs across all the BMCs.  It does this by
    storing the BMC position in the upper byte of the event log ID.  If the
    position can't be obtained, it will encode 0xFF instead.
    
    It is enabled when the new 'use-bmc-pos-in-id' meson option is set to
    true.
    
    There is a new BMCPosMgr class to handle this to prevent adding more
    complexity into log_manager.cpp and to make it easier to test.
    
    Additional details:
    * On startup, read the BMC position from /run/openbmc/bmc_position.
    * If file doesn't exist, or if the value is maxint, consider the
      position to be 0xFF.
    * Store the BMC position in the upper byte of the event log ID, making
      sure to then roll it over at 0x00FFFFFF instead of 0xFFFFFFFF.
    * On new logs, if the position isn't known, try to read it again in case
      the problem preventing it was resolved (e.g. a cable was plugged in).
    
    Tested:
    On BMC 1, where 16777217 = 0x01000001
    ```
    $ busctl tree xyz.openbmc_project.Logging
    └─ /xyz
      └─ /xyz/openbmc_project
        └─ /xyz/openbmc_project/logging
          ├─ /xyz/openbmc_project/logging/entry
          │ └─ /xyz/openbmc_project/logging/entry/16777217
    ```
    
    And the file:
    ```
    /var/lib/phosphor-logging/errors# ls
    16777217
    ```
    
    On BMC 0:
    ```
    ...
          │ ├─ /xyz/openbmc_project/logging/entry/1
    ```
    
    And the file:
    ```
    /var/lib/phosphor-logging/errors# ls
    1
    ```
    
    With no position, where 4278190081 = 0xFF000001
    ```
    $ busctl tree xyz.openbmc_project.Logging
    └─ /xyz
      └─ /xyz/openbmc_project
        └─ /xyz/openbmc_project/logging
          ├─ /xyz/openbmc_project/logging/entry
          │ ├─ /xyz/openbmc_project/logging/entry/4278190081
          │ └─ /xyz/openbmc_project/logging/entry/4278190082
    
    ```
    and
    ```
    /var/lib/phosphor-logging/errors# ls
    4278190081 4278190082
    ```
    
    Change-Id: Ib0677e039acf135fbe811a300b64e1fca140ea75
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

---
+++ bmc_pos_mgr.hpp
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+#pragma once
+
+#include <cstdint>
+#include <filesystem>
+
+namespace phosphor::logging
+{
+
+const std::filesystem::path bmcPositionFile = "/run/openbmc/bmc_position";
+
+/**
+ * @class BMCPosMgr
+ *
+ * Manages encoding the BMC position into the upper byte
+ * of the event log ID.  If the position isn't there, it will
+ * use 0xFF instead.
+ *
+ */
+class BMCPosMgr
+{
+  public:
+    ~BMCPosMgr() = default;
+    BMCPosMgr(const BMCPosMgr&) = delete;
+    BMCPosMgr& operator=(const BMCPosMgr&) = delete;
+    BMCPosMgr(BMCPosMgr&&) = delete;
+    BMCPosMgr& operator=(BMCPosMgr&&) = delete;
+
+    explicit BMCPosMgr(const std::filesystem::path& posFile = bmcPositionFile) :
+        posFile(posFile)
+    {
+        readBMCPosition();
+    }
+
+    /**
+     * @brief Updates entryID with the current BMC position value
+     *        and returns it.
+     *
+     * Rolls over the ID if necessary.
+     *
+     * @param[in] id - The entry ID
+     *
+     * @return The updated entry ID
+     */
+    uint32_t processEntryId(uint32_t id) const;
+
+    /**
+     * @brief Reads the BMC position property from the filesystem
+     *        sets bmcPosition appropriately
+     */
+    void readBMCPosition();
+
+    /**
+     * @brief Says if the current position contains a
+     *        valid position value.
+     */
+    bool isPositionValid() const;
+
+    /**
+     * @brief Returns the BMC position
+     */
+    uint8_t getBMCPosition() const
+    {
+        return bmcPosition;
+    }
+
+  private:
+    /**
+     * @brief Sets the position prefix in the entry ID
+     *
+     * @param[inout] id - The entry ID
+     */
+    void setPrefixInEntryId(uint32_t& id) const;
+
+    /**
+     * @brief If the ID passed in is the last one before a
+     *        rollover, do the rollover.
+     *
+     *        e.g. 0x00FFFFFF will roll over to 0x00000000
+     *
+     * @param[inout] The ID that may be rolled over
+     */
+    void checkEntryIdRollover(uint32_t& id) const;
+
+    /**
+     * @brief The BMC position.
+     *
+     * 0xFF if the position can't be obtained.
+     */
+    uint8_t bmcPosition{};
+
+    /**
+     * @brief The BMC position file
+     */
+    std::filesystem::path posFile;
+};
+
+} // namespace phosphor::logging
