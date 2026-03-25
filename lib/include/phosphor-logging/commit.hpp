e8630b57b2291b7fb7091c6e4dbed639675c2e67
Amithash Prasad <amithash@meta.com>
2026-03-25 15:51:35 -0700

    lg2: commit: Allow users to provide additional data
    Extend commit to allow users to provide additional data
    which can be extended.
    
    Change-Id: I9a0d77234647ae856c503566c1e2936209bbe3df
    Signed-off-by: Amithash Prasad <amithash@meta.com>

--- lib/include/phosphor-logging/commit.hpp
+++ lib/include/phosphor-logging/commit.hpp
@@ -3,20 +3,28 @@
 #include <sdbusplus/async.hpp>
 #include <sdbusplus/exception.hpp>
 
+#include <map>
 #include <optional>
+#include <string>
 
 namespace lg2
 {
+
+using AdditionalData_t = std::map<std::string, std::string>;
+
 /** Commit a generated event/error.
  *
  *  @param e - The event to commit.
+ *  @param overrideLevel - Optional severity level override.
+ *  @param additionalData - Optional additional key-value data to include.
  *  @return The object path of the resulting event.
  *
  *  Note: Similar to elog(), this will use the default dbus connection to
  *  perform the operation.
  */
 auto commit(sdbusplus::exception::generated_event_base&& e,
-            std::optional<int> overrideLevel = std::nullopt)
+            std::optional<int> overrideLevel = std::nullopt,
+            AdditionalData_t additionalData = {})
     -> sdbusplus::message::object_path;
 
 /** Resolve an existing event/error.
@@ -33,11 +41,14 @@ void resolve(const sdbusplus::message::object_path& logPath);
  *
  *  @param ctx - The async context to use.
  *  @param e - The event to commit.
+ *  @param overrideLevel - Optional severity level override.
+ *  @param additionalData - Optional additional key-value data to include.
  *  @return The object path of the resulting event.
  */
 auto commit(sdbusplus::async::context& ctx,
             sdbusplus::exception::generated_event_base&& e,
-            std::optional<int> overrideLevel = std::nullopt)
+            std::optional<int> overrideLevel = std::nullopt,
+            AdditionalData_t additionalData = {})
     -> sdbusplus::async::task<sdbusplus::message::object_path>;
 
 /** Resolve an existing event/error (using async context).
