092ae1e7833b5eb3cc0593aaeced8dc103f42eb5
Manish Tiwari <tmanish.in+openbmc@gmail.com>
2026-03-27 05:57:49 -0500

    Rename meson option to redundant-bmc
    - The existing 'use-bmc-pos-in-id' option name is now too specific
      for what it controls.
    
    - It was originally introduced for encoding the BMC position in the
      event log ID, but it is now also being used to gate broader RBMC
      related behavior. Rename the option to 'redundant-bmc' so it better
      reflects its current purpose and can be reused for RBMC support
      more generally.
    
    Change-Id: Ia4b9562669dffc5ddac19ab73da6a3adbb2a25d1
    Signed-off-by: Manish Tiwari <tmanish.in+openbmc@gmail.com>

--- extensions/openpower-pels/manager.cpp
+++ extensions/openpower-pels/manager.cpp
@@ -71,7 +71,7 @@ void Manager::create(const std::string& message, uint32_t obmcLogID,
     AdditionalData ad{additionalData};
 
     // Extract the latest BMC position value
-    if constexpr (USE_BMC_POS_IN_ID || IS_UNIT_TEST)
+    if constexpr (REDUNDANT_BMC || IS_UNIT_TEST)
     {
         position::extractBMCPostionFromLogID(obmcLogID);
     }
