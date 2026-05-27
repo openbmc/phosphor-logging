2892bc59aec41681fe5122aca80fd42bfd34f984
Riya Sabu <riya.sabu461@gmail.com>
2026-05-26 19:04:29 -0500

    PEL: Multi-chassis support in JSON FFDC callout
    For supporting the multi-chassis system, the chassis number is to be
    associated for each unexpanded FRU location code in JSON callout.
    
    In this case, the chassis number can be explicitly specified using the
    `ChassisNumber` parameter, if none passed running BMC chassis is
    considered.
    
    Example;
    
    ``json
    {
        "LocationCode": "BD00",
        "Priority": "H",
        "ChassisNumber" : 2
    }
    ```
    The unexpanded location code with the prefix will be `Ufcs-Nxx-xxxx`
    for node chassis and `Ufcs-SC0-xxxx` for control unit chassis,
    according to the current convention and passed to the API to expand,
    as well as to fetch the inventory path.
    
    Tested:
    In simics
    
    Case 1: Callout without passing ChassisNumber,
    running BMC chassis is taken.
    
    Callout:
    
    ```json
    {
        "LocationCode": "BD00",
        "Priority": "H"
    }
    ```
    PEL:
    
    '''
    "Callout Section": {
    "Callout Count":        "1",
    "Callouts": [{
    "FRU Type":         "Normal Hardware FRU",
    "Priority":         "Mandatory, replace all with this type as a unit",
    "Location Code":    "UXXX.N00.XXXXXXX-BD00",
    "Part Number":      "XXXXXXX",
    "CCIN":             "XXXX",
    "Serial Number":    "XXXXXXXXXXX"
    }]
    '''
    
    Case 2: Callout passing ChassisNumber
    
    Callout:
    
    ```json
    {
        "LocationCode": "BD00",
        "ChassisNumber" : 0,
        "Priority": "H"
    }
    ```
    
    PEL:
    
    '''
    "Callout Section": {
    "Callout Count":        "1",
    "Callouts": [{
    "FRU Type":         "Normal Hardware FRU",
    "Priority":         "Mandatory, replace all with this type as a unit",
    "Location Code":    "UXXXX.SC0.XXXXXXX-BD00",
    "Part Number":      "XXXXXXX",
    "CCIN":             "XXXX",
    "Serial Number":    "XXXXXXXXXXXX"
    }]
    '''
    
    Change-Id: I878e75ed6da1a13cda845e2f0f9cf23ff877340b
    Signed-off-by: Riya Sabu <riya.sabu461@gmail.com>

--- extensions/openpower-pels/data_interface.cpp
+++ extensions/openpower-pels/data_interface.cpp
@@ -453,7 +453,7 @@ std::string DataInterface::getLocationCode(
 }
 
 std::string DataInterface::addLocationCodePrefix(
-    const std::string& locationCode)
+    const std::string& locationCode, uint16_t chassisNumber)
 {
     static const std::string locationCodePrefix{"Ufcs-"};
 
@@ -461,14 +461,26 @@ std::string DataInterface::addLocationCodePrefix(
     // if it already starts with a U then don't need to do anything.
     if (locationCode.front() != 'U')
     {
-        return locationCodePrefix + locationCode;
+        std::string prefixedLocationCode = locationCode;
+
+        // The prefix is added based on the current format for fetching the
+        // expanded location code Chassis 0 is control unit, chassis 1-12 = node
+        // chassis (numbered N00-N11)
+        prefixedLocationCode =
+            (chassisNumber == 0)
+                ? "SC0-" + locationCode
+                : ((chassisNumber <= 9) ? "N0" : "N") +
+                      std::to_string(chassisNumber - 1) + "-" + locationCode;
+
+        // Finally add the Ufcs- prefix
+        return locationCodePrefix + prefixedLocationCode;
     }
 
     return locationCode;
 }
 
 std::string DataInterface::expandLocationCode(const std::string& locationCode,
-                                              uint16_t /*node*/) const
+                                              uint16_t chassisNumber) const
 {
     // Location codes for connectors are the location code of the FRU they are
     // on, plus a '-Tx' segment.  Remove this last segment before expanding it
@@ -480,7 +492,8 @@ std::string DataInterface::expandLocationCode(const std::string& locationCode,
         _bus.new_method_call(service_name::vpdManager, object_path::vpdManager,
                              interface::vpdManager, "GetExpandedLocationCode");
 
-    method.append(addLocationCodePrefix(baseLoc), static_cast<uint16_t>(0));
+    method.append(addLocationCodePrefix(baseLoc, chassisNumber),
+                  static_cast<uint16_t>(0));
 
     auto reply = _bus.call(method, dbusTimeout);
 
@@ -495,7 +508,8 @@ std::string DataInterface::expandLocationCode(const std::string& locationCode,
 }
 
 std::vector<std::string> DataInterface::getInventoryFromLocCode(
-    const std::string& locationCode, uint16_t node, bool expanded) const
+    const std::string& locationCode, uint16_t chassisNumber,
+    bool expanded) const
 {
     std::string methodName = expanded ? "GetFRUsByExpandedLocationCode"
                                       : "GetFRUsByUnexpandedLocationCode";
@@ -516,7 +530,8 @@ std::vector<std::string> DataInterface::getInventoryFromLocCode(
     }
     else
     {
-        method.append(addLocationCodePrefix(baseLoc), node);
+        method.append(addLocationCodePrefix(baseLoc, chassisNumber),
+                      static_cast<uint16_t>(0));
     }
 
     auto reply = _bus.call(method, dbusTimeout);
