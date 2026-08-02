bc70de4c896989b9287607beb2bf4de92a210097
Jayanth Othayoth <ojayanth@gmail.com>
2026-08-02 11:24:28 -0500

    logging: defer entry object-added signal emission
    Move log entry object-added signal emission from Entry construction
    to the logging manager.
    
    Deferring signal emission allows future changes to perform additional
    log entry initialization before the entry becomes visible on D-Bus.
    This is required to support plugin-provided interfaces that must be
    fully constructed before clients observe the associated log entry.
    
    No functional behavior changes are intended by this commit.
    
    Change-Id: I5e25ffbbe3cccfde79ac87d5cc9fc948afe36624
    Signed-off-by: Jayanth Othayoth <ojayanth@gmail.com>

--- log_manager.cpp
+++ log_manager.cpp
@@ -300,6 +300,8 @@ auto Manager::createEntry(std::string errMsg, Entry::Level errLvl,
     serialize(*e);
     serializeJSON(*e);
 
+    e->emit_object_added();
+
     if (isQuiesceOnErrorEnabled() && (errLvl < Entry::sevLowerLimit) &&
         isCalloutPresent(*e))
     {
